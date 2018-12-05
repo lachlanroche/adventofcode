1a788e08b0b0428566d9103411bea9cd66455022
Lachlan Roche <hello@lachlan.roche.name>
2018-12-06 00:00:50 +1000

    Utils - read strgins or numbers from source

--- adventofcode2018.playground/Sources/Utils.swift
+++ adventofcode2018.playground/Sources/Utils.swift
@@ -1,17 +1,27 @@
 import Foundation
 
-public func numbersFromFile(named: String) -> [Int]
+public func stringsFromFile(named: String) -> [String]
 {
     guard let path = Bundle.main.path(forResource: named, ofType: "txt") else { return [] }
     guard let data = FileManager.default.contents(atPath: path) else { return [] }
     guard let content = String(data:data, encoding:String.Encoding.utf8) else { return [] }
-
+    
     return content.components(separatedBy: CharacterSet.newlines)
+}
+
+public func numbersFromFile(named: String) -> [Int]
+{
+    return stringsFromFile(named: named)
         .compactMap(Int.init)
+}
+
+public func stringsFromString(_ str: String) -> [String]
+{
+    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: ",")))
 }
 
 public func numbersFromString(_ str: String) -> [Int]
 {
-    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: ",")))
+    return stringsFromString(str)
         .compactMap(Int.init)
 }
