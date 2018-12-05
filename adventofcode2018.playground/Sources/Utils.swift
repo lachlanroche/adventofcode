2a7732d4d97ae46f635f9ec0588f2110d37b5efe
Lachlan Roche <hello@lachlan.roche.name>
2018-12-05 22:39:33 +1000

    Utils - numbersFromStrign()

--- adventofcode2018.playground/Sources/Utils.swift
+++ adventofcode2018.playground/Sources/Utils.swift
@@ -6,6 +6,12 @@ public func numbersFromFile(named: String) -> [Int]
     guard let data = FileManager.default.contents(atPath: path) else { return [] }
     guard let content = String(data:data, encoding:String.Encoding.utf8) else { return [] }
 
-    return content.components(separatedBy: .newlines)
+    return content.components(separatedBy: CharacterSet.newlines)
+        .flatMap(Int.init)
+}
+
+public func numbersFromString(_ str: String) -> [Int]
+{
+    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: ",")))
         .flatMap(Int.init)
 }
