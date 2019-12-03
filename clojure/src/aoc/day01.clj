604d32e9fdc3370529a144bc5fd386b275fba2ba
Lachlan Roche <hello@lachlan.roche.name>
2019-12-04 00:10:29 +1000

    day01 part1

--- clojure/src/aoc/day01.clj
+++ clojure/src/aoc/day01.clj
@@ -5,8 +5,19 @@
   (- (quot mass 3) 2))
 
 
+(defn part1
+  []
+  (->>
+   "aoc/day01.txt"
+   clojure.java.io/resource
+   slurp
+   clojure.string/split-lines
+   (map #(Integer/parseInt %))
+   (reduce #(+ % (fuel-for-mass %2)) 0)))
+
   (fuel-for-mass 12)
   (fuel-for-mass 14)
   (fuel-for-mass 1969)
   (fuel-for-mass 100756)
+  (part1)
 
