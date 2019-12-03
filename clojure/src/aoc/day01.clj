d717fa95ffac042c97e1e6ede4850dc1478b7f98
Lachlan Roche <hello@lachlan.roche.name>
2019-12-04 00:18:50 +1000

    day01 cleanup

--- clojure/src/aoc/day01.clj
+++ clojure/src/aoc/day01.clj
@@ -25,18 +25,13 @@
 
 (defn part1
   []
-  (->>
-   "aoc/day01.txt"
-   clojure.java.io/resource
-   slurp
-   clojure.string/split-lines
-   (map #(Integer/parseInt %))
-   (reduce #(+ % (fuel-for-mass %2)) 0)))
+  (fuel-for-spacecraft fuel-for-mass))
 
 (defn part2
   []
   (fuel-for-spacecraft fuel-for-module))
 
+(comment
   (fuel-for-mass 12)
   (fuel-for-mass 14)
   (fuel-for-mass 1969)
@@ -46,3 +41,4 @@
   (fuel-for-module 1969)
   (fuel-for-module 100756)
   (part2)
+)
