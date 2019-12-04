1c3636ed7246ec122160234856430bf46ebfe384
Lachlan Roche <hello@lachlan.roche.name>
2019-12-05 02:07:20 +1000

    day03 part2

--- clojure/src/aoc/day03.clj
+++ clojure/src/aoc/day03.clj
@@ -70,6 +70,32 @@
       (map #(clojure.string/split % #","))
       (apply wire-cross-distance)))
 
+(defn wire-delay-cost-map
+  [points]
+  (->> points
+       (map-indexed #(vector %2 (+ 1 %1)))
+       (into {})))
+
+(defn wire-least-delay-intersection
+  [s1 s2]
+  (let [w1 (wire-points s1)
+        w2 (wire-points s2)
+        joins (clojure.set/intersection (set w1) (set w2))
+        cost1 (-> w1 wire-delay-cost-map (select-keys (seq joins)))
+        cost2 (-> w2 wire-delay-cost-map (select-keys (seq joins)))
+        cheapest (apply min-key #(+ (get cost1 %) (get cost2 %)) (seq joins))
+        ]
+    (+ (get cost1 cheapest) (get cost2 cheapest))))
+
+(defn part2
+  []
+  (->> "aoc/day03.txt"
+      clojure.java.io/resource
+      slurp
+      clojure.string/split-lines
+      (map #(clojure.string/split % #","))
+      (apply wire-least-delay-intersection)))
+
 (op-right [0 0] 2)
 (op-left [0 0] 2)
 (op-up [0 0] 2)
@@ -81,5 +107,14 @@
  ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
  ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
 (wire-cross-distance
+ ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
+ ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
+(wire-least-delay-intersection
+ ["R8","U5","L5","D3"]
+ ["U7","R6","D4","L4"])
+(wire-least-delay-intersection
+ ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
+ ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
+(wire-least-delay-intersection
  ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
  ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
