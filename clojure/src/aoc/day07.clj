4fb5194d137d0ae79654cf16bae0fa2242f8dbb3
Lachlan Roche <hello@lachlan.roche.name>
2019-12-07 22:49:29 +1000

    day07 import intcode source from day05

--- clojure/src/aoc/day07.clj
+++ clojure/src/aoc/day07.clj
@@ -1,20 +1,180 @@
 (ns aoc.day07
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
-            [aoc.day05 :as ic]
             [clojure.math.combinatorics :as combo]))
+
+(defn cpu-peek
+  [mem mode a]
+  #_(tap> ["peek" mode a])
+  (cond
+    (= mode 0)
+    (get mem a)
+    (= mode 1)
+    a
+    ))
+
+(defn cpu-poke-assoc
+  [mem a v]
+  (if (< a (count mem))
+    (assoc mem a v)
+    (let [mem (apply conj mem (repeat (- a (count mem)) 0))]
+      (assoc mem a v))))
+
+(defn cpu-poke
+  [mem mode a val]
+  #_(tap> ["poke" mode a val])
+  #_(tap> mem)
+  (cond
+    (= mode 0)
+    (cpu-poke-assoc mem (get mem a) val)
+    (= mode 1)
+    (cpu-poke-assoc mem a val)))
+
+(defn cpu-input
+  [in]
+  [(first in) (vec (rest in))])
+
+(defn cpu-output
+  [out a]
+  (conj out a))
+
+(defn op-add
+  [[a b z] [ma mb mz] [mem in out ip]]
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)
+        result (+ a b)
+        mem (cpu-poke mem 1 z result)]
+    [mem in out (+ 4 ip)]))
+
+(defn op-mul
+  [[a b z] [ma mb mz] [mem in out ip]]
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)
+        result (* a b)
+        mem (cpu-poke mem 1 z result)]
+    [mem in out (+ 4 ip)]))
+
+(defn op-input
+  [[z] [mz] [mem in out ip]]
+  (let [[result in] (cpu-input in)
+        mem (cpu-poke mem 1 z result)]
+    [mem in out (+ 2 ip)]))
+
+(defn op-output
+  [[z] [mz] [mem in out ip]]
+  (let [result (cpu-peek mem mz z)
+        out (cpu-output out result)]
+    [mem in out (+ 2 ip)]))
+
+(defn op-jump-if-true
+  [[a b] [ma mb] [mem in out ip]]
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)]
+    (if (not= 0 a)
+      [mem in out b]
+      [mem in out (+ 3 ip)])))
+
+(defn op-jump-if-false
+  [[a b] [ma mb] [mem in out ip]]
+  #_(tap> ["jif" [a b] [ma mb] ip])
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)]
+    (if (= 0 a)
+      [mem in out b]
+      [mem in out (+ 3 ip)])))
+
+(defn op-less-than
+  [[a b z] [ma mb mz] [mem in out ip]]
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)
+        result (if (< a b) 1 0)
+        mem (cpu-poke mem 1 z result)]
+  [mem in out (+ 4 ip)]))
+
+(defn op-equals
+  [[a b z] [ma mb mz] [mem in out ip]]
+  (let [a (cpu-peek mem ma a)
+        b (cpu-peek mem mb b)
+        result (if (= a b) 1 0)
+        mem (cpu-poke mem 1 z result)]
+  [mem in out (+ 4 ip)]))
+
+(defn op-halt
+  [_ _ _]
+  nil)
+
+(def op-map
+  {1 {:fn op-add :argc 3}
+   2 {:fn op-mul :argc 3}
+   3 {:fn op-input :argc 1}
+   4 {:fn op-output :argc 1}
+   5 {:fn op-jump-if-true :argc 2}
+   6 {:fn op-jump-if-false :argc 2}
+   7 {:fn op-less-than :argc 3}
+   8 {:fn op-equals :argc 3}
+   99 {:fn op-halt :argc 0}
+   })
+
+(defn cpu-decode
+  [raw-opcode]
+  #_(tap> ["decode" raw-opcode])
+  (let [opcode (rem raw-opcode 100)
+        ma (rem (quot raw-opcode 100) 10)
+        mb (rem (quot raw-opcode 1000) 10)
+        mc (rem (quot raw-opcode 10000) 10)
+        op (get op-map opcode (get op-map 99))
+        op-fn (:fn op)
+        op-argc (:argc op)
+        ]
+    {:raw opcode
+     :mode [ma mb mc]
+     :argc op-argc
+     :fn op-fn
+     }))
+
+(defn program
+  ([mio]
+   (program mio 0))
+  ([[mem in out] offset]
+   (let [op (cpu-decode (get mem offset))
+         op-fn (:fn op)
+         op-argc (:argc op)
+         op-mode (:mode op)]
+     #_(tap> op)
+     #_(tap> offset)
+     #_(tap> (vec (take op-argc (drop (+ 1 offset) mem))))
+     (if (= op-halt op-fn)
+       [mem nil out offset]
+       (let [[mem in out offset] (op-fn (vec (take op-argc (drop (+ 1 offset) mem))) op-mode [mem in out offset])]
+         #_(tap> [mem in out])
+         (recur [mem in out] offset))))))
+
+(defn program-compile
+  [s]
+  (let [numbers (string/split s #"[,\n]")
+        mem (vec (map #(Integer/parseInt %) numbers))
+        ]
+    mem))
+
+(defn run-program
+ ([s]
+  (run-program s [] []))
+ ([s in]
+  (run-program s in []))
+ ([s in out]
+  (program [(program-compile s) in out])))
 
 (defn run-program-phase
   [prog phase in]
   (let [in (concat [phase] in)
-        out (ic/program [prog in []])]
+        out (program [prog in []])]
     out))
 
 (defn run-program-phase-list
   [prog phases in]
   (let [phase (first phases)
         phases (rest phases)
-        [_ _ out _] (ic/run-program-phase prog phase in)
+        [_ _ out _] (run-program-phase prog phase in)
         ]
     (if (empty? phases)
       out
@@ -28,7 +188,7 @@
 
 (defn part1-run
   [s]
-  (let [p (ic/program-compile s)
+  (let [p (program-compile s)
         outmap (run-programm-all-phases p [0 1 2 3 4])
         outmap (sort #(> (ffirst %1) (ffirst %2)) outmap)
         ]
