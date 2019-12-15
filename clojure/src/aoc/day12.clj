54d34c44fd95c2f832f9e8bffc0a1db4360f40a5
Lachlan Roche <hello@lachlan.roche.name>
2019-12-16 00:20:11 +1000

    day12 part2

--- clojure/src/aoc/day12.clj
+++ clojure/src/aoc/day12.clj
@@ -65,12 +65,65 @@
   (let [moons [(make-moon -3 15 -11) (make-moon 3 13 -19) (make-moon -13 18 -2) (make-moon 6 0 -1)]]
     (part1-impl 1000 moons)))
 
+
+(defn gcd
+  [a b]
+  (if (zero? b)
+    a
+    (recur b, (mod a b))))
+
+(defn lcm
+  [a b]
+  (/ (* a b) (gcd a b)))
+
+(defn lcmv [v] (reduce lcm v))
+
+(defn check-cycle
+  [moons initial n]
+  (and (= (get-in moons [0 :velocity n]) (get-in initial [0 :velocity n]))
+       (= (get-in moons [0 :position n]) (get-in initial [0 :position n]))
+       (= (get-in moons [1 :velocity n]) (get-in initial [1 :velocity n]))
+       (= (get-in moons [1 :position n]) (get-in initial [1 :position n]))
+       (= (get-in moons [2 :velocity n]) (get-in initial [2 :velocity n]))
+       (= (get-in moons [2 :position n]) (get-in initial [2 :position n]))
+       (= (get-in moons [3 :velocity n]) (get-in initial [3 :velocity n]))
+       (= (get-in moons [3 :position n]) (get-in initial [3 :position n]))))
+
+(defn part2-impl
+  [moons]
+  (let [initial moons
+        ]
+    (loop [i 1 moons moons cycle [nil nil nil]]
+      (cond
+        (reduce #(and %1 (not (nil? %2))) true cycle)
+        (lcmv cycle)
+        :else
+        (let [moons (physics-step moons)
+              [m1 m2 m3 m4] moons
+              [cx cy cz] cycle
+              cx (cond (not (nil? cx)) cx (check-cycle moons initial 0) i :else nil)
+              cy (cond (not (nil? cy)) cy (check-cycle moons initial 1) i :else nil)
+              cz (cond (not (nil? cz)) cz (check-cycle moons initial 2) i :else nil)
+              ]
+          (recur (inc i) moons [cx cy cz]))))))
+
+(defn part2
+  []
+  (let [moons [(make-moon -3 15 -11) (make-moon 3 13 -19) (make-moon -13 18 -2) (make-moon 6 0 -1)]]
+    (part2-impl moons)))
+
 (comment
 (part1)
+(part2)
 (->> [(make-moon -1 0 2) (make-moon 2 -10 -7) (make-moon 4 -8 8) (make-moon 3 5 -1)]
      (part1-impl 10)
      )
 (->> [(make-moon -8 -10 0) (make-moon 5 5 10) (make-moon 2 -7 3) (make-moon 9 -8 -3)]
      (part1-impl 100)
+     )
+(->> [(make-moon -1 0 2) (make-moon 2 -10 -7) (make-moon 4 -8 8) (make-moon 3 5 -1)]
+     (part2-impl))
+(->> [(make-moon -8 -10 0) (make-moon 5 5 10) (make-moon 2 -7 3) (make-moon 9 -8 -3)]
+     (part2-impl)
      )
 )
