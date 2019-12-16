85120fb7e1a38287ba4c47b6732eb871b76556c7
Lachlan Roche <hello@lachlan.roche.name>
2019-12-16 22:52:12 +1000

    day16 part2

--- clojure/src/aoc/day16.clj
+++ clojure/src/aoc/day16.clj
@@ -45,9 +45,43 @@
               slurp)]
     (fft (parse s) 100)))
 
+
+(defn last-digit
+  [x]
+  (-> x
+      Math/abs
+      (mod 10)))
+
+(defn part2-impl
+  [end]
+  (let [list-sum (reduce + end)]
+    (loop [end end out [] r list-sum]
+      (if (empty? end)
+        (reverse out)
+        (let [r1 (- r (first end))]
+          (recur (rest end) (cons (last-digit r) out) r1))))))
+
+(defn part2
+  []
+  (let [s (-> "aoc/day16.txt"
+              io/resource
+              slurp)
+        signal (->> s
+                    parse
+                    (repeat 10000)
+                    flatten)
+        offset (->> signal
+                    (take 7)
+                    (reduce #(+ %2 (* 10 %1)) 0))
+        ]
+    (loop [i 0 xs (drop offset signal)]
+      (if (= i 100)
+        (take 8 xs)
+        (recur (inc i) (part2-impl xs))))))
+
 (comment
 (part1)
-
+(part2)
 (fft [1 2 3 4 5 6 7 8] 4)
 (fft (parse "") 100)
 (= (parse "24176176") (fft (parse "80871224585914546619083218645595") 100))
