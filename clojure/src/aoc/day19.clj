a1a6e71dc494496c47b827ce7deedfcf38785456
Lachlan Roche <hello@lachlan.roche.name>
2019-12-19 20:11:58 +1000

    day19 part2

--- clojure/src/aoc/day19.clj
+++ clojure/src/aoc/day19.clj
@@ -2,8 +2,7 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
             [clojure.set :as set]
-            [aoc.day09 :as ic]
-            [astar.core :as astar]))
+            [aoc.day09 :as ic]))
 
 (defn load-program
   []
@@ -31,7 +30,28 @@
           :else
           (recur total (inc x) y))))))
 
+(defn santa-catchable?
+  [prog x y]
+  (reduce #(and %1 (affected? prog %2)) true [[x y] [x (+ 99 y)] [(+ 99 x) y] [(+ 99 x) (+ 99 y)]]))
+
+(defn part2
+  []
+  (let [prog (load-program)
+        affected? (partial affected? prog)
+        catchable? (partial santa-catchable? prog)]
+    (loop [x 5 y 6]
+      (let [ax x
+            ay (- y 99)
+            success? (if (and (pos? ax) (pos? ay)) (catchable? ax ay) false)]
+        (cond
+          success?
+          (+ ay (* 10000 ax))
+          (affected? [x (inc y)])
+          (recur x (inc y))
+          :else
+          (recur (inc x) y))))))
+
 (comment
 (part1)
+(part2)
 )
-
