54904b595308082d6346a209bbabb602178273c2
Lachlan Roche <hello@lachlan.roche.name>
2020-01-04 18:43:23 +1000

    2015 day01 part2

--- aoc15.playground/Pages/Day01.xcplaygroundpage/Contents.swift
+++ aoc15.playground/Pages/Day01.xcplaygroundpage/Contents.swift
@@ -20,10 +20,33 @@ func part1(s: String) -> Int {
     }
 }
 
+func part2(s: String) -> Int {
+    var floor = 0
+    for (i, ch) in s.enumerated() {
+        switch Direction(rawValue: ch) {
+        case nil:
+            continue
+        case .some(.up):
+            floor = floor + 1
+            break
+        case .some(.down):
+            floor = floor - 1
+            break
+        }
+        if floor == -1 {
+            return i + 1
+        }
+    }
+    return 0
+}
+
 ["(())", "()()", "(((", "(()(()(", "))(((((", "())", "))(", ")))", ")())())"].map(part1)
 
 let url = Bundle.main.url(forResource: "input01", withExtension: "txt")
 let data = try Data(contentsOf: url!)
 part1(s: String(data: data, encoding: .utf8)!)
+
+[")", "()())"].map(part2)
+part2(s: String(data: data, encoding: .utf8)!)
 
 //: [Next](@next)
