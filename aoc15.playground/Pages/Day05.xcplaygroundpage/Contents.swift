3968f05e9571af8e36bc90cb161956b8275ecade
Lachlan Roche <hello@lachlan.roche.name>
2020-01-04 20:49:56 +1000

    2015 day05 part1

---
+++ aoc15.playground/Pages/Day05.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+import Day05_PageSources
+
+"ugknbfddgicrmopn".isNice()
+"aaa".isNice()
+"jchzalrnumimnmhp".isNice()
+"haegwjzuvuyypxyu".isNice()
+"dvszwmarrgswjxmb".isNice()
+part1()
+
+//: [Next](@next)
---
+++ aoc15.playground/Pages/Day05.xcplaygroundpage/Sources/Day05.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+public func inputstring() -> String {
+    let url = Bundle.main.url(forResource: "input05", withExtension: "txt")
+    let data = try? Data(contentsOf: url!)
+    return String(data: data!, encoding: .utf8)!
+}
+
+extension String {
+    func hasThreeVowels() -> Bool {
+        let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
+        var count = 0
+
+        for (_, ch) in self.enumerated() {
+            if vowels.contains(ch) {
+                count = count + 1
+            }
+            
+            if count == 3 {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func hasRepeats() -> Bool {
+        var prev =  Character("_")
+
+        for (_, ch) in self.enumerated() {
+            if prev == ch {
+                return true
+            }
+            prev = ch
+        }
+        
+        return false
+    }
+    
+    func hasStopWords() -> Bool {
+        let stop: Set<String> = ["ab", "cd", "pq", "xy"]
+        var prev =  Character("_")
+
+        for (_, ch) in self.enumerated() {
+            let word = String([prev, ch])
+            prev = ch
+            if stop.contains(word) {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
+
+public extension String {
+    func isNice() -> Bool {
+        return hasThreeVowels() && hasRepeats() && !hasStopWords()
+    }
+}
+
+public func part1() -> Int {
+    return inputstring()
+        .components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+        .reduce(0) { (acc, str) in
+            if str.isNice() {
+                return acc + 1
+            } else {
+                return acc
+            }
+        }
+}
