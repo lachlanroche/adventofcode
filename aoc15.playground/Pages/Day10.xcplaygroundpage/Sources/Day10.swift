3e45e9cfb4d1d24d84192229d1f78fb239dfc34a
Lachlan Roche <hello@lachlan.roche.name>
2020-01-05 18:04:24 +1000

    2015 day10 part1

---
+++ aoc15.playground/Pages/Day10.xcplaygroundpage/Sources/Day10.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public func lookAndSay(input: String) -> String {
+    func say(result: inout [Character], char: Character, count: Int) {
+        result.append(contentsOf: String(count))
+        result.append(char)
+    }
+    
+    var result = [Character]()
+    var count = 1
+    var prev: Character? = nil
+    for ch in input {
+        if prev == ch {
+            count = 1 + count
+        } else if let prev = prev {
+            say(result: &result, char: prev, count: count)
+            count = 1
+        }
+        prev = ch
+    }
+    if let prev = prev {
+        say(result: &result, char: prev, count: count)
+    }
+    
+    return String(result)
+}
+
+public func part1() -> Int {
+    var s = "1113222113"
+    for _ in 1...40 {
+        s = lookAndSay(input: s)
+    }
+    return s.count
+}
