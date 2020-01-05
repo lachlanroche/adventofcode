340ab02f32e01e671b4838c6829bb54aa65c3d89
Lachlan Roche <hello@lachlan.roche.name>
2020-01-05 18:55:12 +1000

    2015 day11 part1

---
+++ aoc15.playground/Pages/Day11.xcplaygroundpage/Sources/Day11.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+let letters: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+public
+extension Character {
+    func letterIndex() -> Int {
+        return Int(self.asciiValue!) - 97
+    }
+    func increment() -> Character {
+        return letters[(1 + letterIndex()) % 26]
+    }
+}
+
+public
+extension Array where Element == Character {
+    mutating func increment() {
+        var index = count - 1
+        
+        while true {
+            self[index] = self[index].increment()
+            guard self[index] == "a" else { return }
+            guard index > 0 else { break }
+            index = index - 1
+        }
+        
+        insert("a", at: 0)
+    }
+    
+    func noStopLetters() -> Bool {
+        for ch in self {
+            switch ch {
+            case "i", "o", "l":
+                return false
+            default:
+                break
+            }
+        }
+        return true
+    }
+    
+    func hasIncreasingStraight() -> Bool {
+        var pprev: UInt8 = 0
+        var prev: UInt8 = 0
+        
+        for ch in self {
+            let val = ch.asciiValue!
+            if val == prev && prev == pprev {
+                return true
+            }
+            
+            pprev = 1 + prev
+            prev = 1 + val
+        }
+        
+        return false
+    }
+    
+    func hasTwoPairs() -> Bool {
+        var pairs = Set<Character>()
+        
+        var prev: Character = "_"
+        for ch in self {
+            if ch == prev {
+                pairs.insert(ch)
+                if pairs.count >= 2 {
+                    return true
+                }
+            }
+            prev = ch
+        }
+        
+        return false
+    }
+    
+    func isValid1() -> Bool {
+        return noStopLetters() && hasIncreasingStraight() && hasTwoPairs()
+    }
+}
+
+public func part1() -> String {
+    var password = Array("hepxcrrq")
+    
+    repeat {
+        password.increment()
+    } while !password.isValid1()
+    
+    return String(password)
+}
