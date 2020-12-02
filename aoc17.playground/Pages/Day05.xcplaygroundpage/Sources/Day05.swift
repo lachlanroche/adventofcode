ab8d06318a6a9c509cfe08152c80d49e57e4e586
Lachlan Roche <hello@lachlan.roche.name>
2020-12-02 21:47:23 +1000

    2017 day05 part2

--- aoc17.playground/Pages/Day05.xcplaygroundpage/Sources/Day05.swift
+++ aoc17.playground/Pages/Day05.xcplaygroundpage/Sources/Day05.swift
@@ -19,3 +19,21 @@ public func part1() -> Int {
         i += 1
     }
 }
+
+public func part2() -> Int {
+    var ram = inputData()
+    var pc = 0
+    var i = 0
+    while true {
+        guard pc >= 0 else { return i }
+        guard pc < ram.count else { return i }
+        let delta = ram[pc]
+        if delta >= 3 {
+            ram[pc] -= 1
+        } else {
+            ram[pc] += 1
+        }
+        pc += delta
+        i += 1
+    }
+}
