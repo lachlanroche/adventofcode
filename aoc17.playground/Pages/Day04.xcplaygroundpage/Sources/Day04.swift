5c75ba781fad42ee2247ab89588fe16ca4402a55
Lachlan Roche <hello@lachlan.roche.name>
2020-12-02 21:28:17 +1000

    2017 day04 part2

--- aoc17.playground/Pages/Day04.xcplaygroundpage/Sources/Day04.swift
+++ aoc17.playground/Pages/Day04.xcplaygroundpage/Sources/Day04.swift
@@ -17,3 +17,16 @@ public func part1() -> Int {
     }
     return inputData().filter{ isValid($0) }.count
 }
+
+public func part2() -> Int {
+    func isValid(_ words: [String]) -> Bool {
+        var seen: Set<String> = []
+        for w in words {
+            let hash = String(w.sorted())
+            guard !seen.contains(hash) else { return false }
+            seen.insert(hash)
+        }
+        return true
+    }
+    return inputData().filter{ isValid($0) }.count
+}
