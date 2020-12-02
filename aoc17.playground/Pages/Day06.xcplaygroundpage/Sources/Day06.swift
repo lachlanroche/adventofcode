f322a0b55b106f6619f4fbf28a29afb76fcb2471
Lachlan Roche <hello@lachlan.roche.name>
2020-12-02 22:13:00 +1000

    2017 day06 part2

--- aoc17.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
+++ aoc17.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
@@ -41,3 +41,40 @@ public func part1() -> Int {
         i += 1
     }
 }
+
+public func part2() -> Int {
+    func reallocate() {
+        let size = ram.count
+        var max = 0
+        var n = 0
+        for i in 0..<size {
+            if ram[i] > max {
+                max = ram[i]
+                n = i
+            }
+        }
+        
+        var blocks = ram[n]
+        ram[n] = 0
+        
+        while blocks > 0 {
+            n = (1 + n) % size
+            ram[n] += 1
+            blocks -= 1
+        }
+    }
+    
+    var ram = inputData()
+    
+    var seen: Dictionary<String, Int> = [:]
+    seen[ram.description] = 0
+    var i = 1
+    while true {
+        reallocate()
+        
+        let hash = ram.description
+        guard nil == seen[hash] else { return 1 + i - seen[hash]! }
+        i += 1
+        seen[hash] = i
+    }
+}
