673b936d70c26a6b87b347e0f4efb84579379021
Lachlan Roche <hello@lachlan.roche.name>
2020-12-02 21:46:01 +1000

    2017 day05 part1

---
+++ aoc17.playground/Pages/Day05.xcplaygroundpage/Sources/Day05.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+func inputData() -> [Int] {
+    return stringsFromFile(named: "input")
+        .compactMap{ Int($0) }
+}
+
+public func part1() -> Int {
+    var ram = inputData()
+    var pc = 0
+    var i = 0
+    while true {
+        guard pc >= 0 else { return i }
+        guard pc < ram.count else { return i }
+        let delta = ram[pc]
+        ram[pc] += 1
+        pc += delta
+        i += 1
+    }
+}
