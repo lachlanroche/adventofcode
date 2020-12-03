3ac021e2a706c811fb6e2758e4011196facfcea3
Lachlan Roche <hello@lachlan.roche.name>
2020-12-03 15:53:49 +1000

    2020 day03 part1

---
+++ aoc20/src/aoc/day03.clj
@@ -0,0 +1,45 @@
+(ns aoc20.day03)
+
+(defn input-data
+  []
+  (let [s (->> "aoc/day03.txt"
+                  clojure.java.io/resource
+                  slurp)
+        ]
+    (loop [s s x 0 y 0 canvas #{} size [0 0]]
+      (if (empty? s)
+        {:canvas canvas :size size}
+        (let [ch (first s)
+              tree? (= \# ch)
+              [max-x max-y] size
+              canvas (if (not tree?) canvas (conj canvas [x y]))]
+          (cond
+            (= ch \newline)
+            (recur (rest s) 0 (inc y) canvas size)
+            :else
+            (let [size [(max max-x x) (max max-y y)]]
+              (recur (rest s) (inc x) y canvas size))))))))
+
+(defn tree?
+  [canvas [x y]]
+  (not (nil? (get canvas [x y]))))
+
+(defn part1
+  []
+  (let [{:keys [canvas size]} (input-data)
+        [max-x max-y] size]
+    (loop [x 0 y 0 t 0]
+      (let [x_ (mod (+ 3 x) (inc max-x))
+            y_ (+ 1 y)]
+        (tap> [[x y] (tree? canvas [x y])])
+        (cond
+          (> y max-y)
+          t
+          (tree? canvas [x y])
+          (recur x_ y_ (inc t))
+          :true
+          (recur x_ y_ t))))))
+
+(comment
+  (part1)
+)
