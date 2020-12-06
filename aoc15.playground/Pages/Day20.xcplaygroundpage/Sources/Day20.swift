71dd630d9ba7f51817cb3e2044dd415b8a5cf5b5
Lachlan Roche <hello@lachlan.roche.name>
2020-12-06 21:01:52 +1000

    2015 day20 part2

--- aoc15.playground/Pages/Day20.xcplaygroundpage/Sources/Day20.swift
+++ aoc15.playground/Pages/Day20.xcplaygroundpage/Sources/Day20.swift
@@ -14,3 +14,20 @@ public func part1() -> Int {
     }
     return 0
 }
+
+public func part2() -> Int {
+    for i in 850_000... {
+        var count = 0
+        for e in (i/50 - 1)...i where 0 == i % e {
+            if i <= e * 50 {
+                count += 11 * e
+            } else {
+                break
+            }
+        }
+        if count >= 36_000_000 {
+            return i
+        }
+    }
+    return 0
+}
