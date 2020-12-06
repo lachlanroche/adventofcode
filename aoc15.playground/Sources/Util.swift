99c90c4a5d3935a75fefd995e59e939773757433
Lachlan Roche <hello@lachlan.roche.name>
2020-12-06 16:37:30 +1000

    2015 utils

---
+++ aoc15.playground/Sources/Util.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public func stringsFromFile(named: String = "input") -> [String]
+{
+    guard let path = Bundle.main.path(forResource: named, ofType: "txt") else { return [] }
+    guard let data = FileManager.default.contents(atPath: path) else { return [] }
+    guard let content = String(data:data, encoding:String.Encoding.utf8) else { return [] }
+    
+    return content.components(separatedBy: CharacterSet.newlines)
+}
+
+public func numbersFromFile(named: String) -> [Int]
+{
+    return stringsFromFile(named: named)
+        .compactMap(Int.init)
+}
