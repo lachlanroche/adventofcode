0daf1bbbf6834056b44afe9619621fe04e4a8a2a
Lachlan Roche <hello@lachlan.roche.name>
2020-12-06 15:17:15 +1000

    2020 day06 part1

---
+++ aoc20/src/aoc/day06.clj
@@ -0,0 +1,34 @@
+(ns aoc20.day06)
+
+(defn input-lines
+  []
+  (let [s (->> "aoc/day06.txt"
+               clojure.java.io/resource
+               slurp
+               clojure.string/split-lines)
+        ]
+    s))
+
+(defn input-data
+  []
+  (loop [pp [] p #{} ss (input-lines)]
+    (let [s (first ss)
+          ss (rest ss)]
+      (cond
+        (nil? s)
+        (conj pp p)
+        (= "" s)
+        (recur (conj pp p) [] ss)
+        :else
+        (recur pp (conj p (set s)) ss)))))
+
+(defn part1
+  []
+  (->> (input-data)
+       (map #(apply clojure.set/union %))
+       (map count)
+       (reduce +)))
+
+(comment
+  (part1)
+)
