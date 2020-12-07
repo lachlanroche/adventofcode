455f809d11c3c0ee9cb06979a8b0d07156a66756
Lachlan Roche <hello@lachlan.roche.name>
2020-12-07 21:32:15 +1000

    2015 day12 part2

--- aoc15.playground/Pages/Day12.xcplaygroundpage/Sources/Day12.swift
+++ aoc15.playground/Pages/Day12.xcplaygroundpage/Sources/Day12.swift
@@ -16,3 +16,36 @@ public func part1() -> Int {
 
     return total
 }
+
+func sum(_ obj: AnyObject) -> Int {
+    if let num = obj as? Int {
+        return num
+    }
+    if let num = obj as? String {
+        return 0
+    }
+    if let arr = obj as? NSArray {
+        var arr_total = 0
+        for arr_i in 0..<arr.count {
+            arr_total += sum(arr[arr_i] as! AnyObject)
+        }
+        return arr_total
+    }
+    if let dict = obj as? NSDictionary {
+        if dict.allValues.contains{ ($0 as? String) == "red" } { return 0 }
+        var dict_total = 0
+        for v in dict.allValues {
+            dict_total += sum(v as! AnyObject)
+        }
+        return dict_total
+    }
+    
+    return 0
+}
+
+public func part2() -> Int {
+    let s = stringsFromFile(named: "input")[0]
+    let data = s.data(using: .ascii)!
+    let json = try! JSONSerialization.jsonObject(with: data, options: []) as! AnyObject
+    return sum(json)
+}
