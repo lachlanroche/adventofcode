e9c3fea1dbebff4296187dcacf23d2f62aa048e1
Lachlan Roche <hello@lachlan.roche.name>
2020-12-07 20:07:23 +1000

    2015 day15 part2

--- aoc15.playground/Pages/Day15.xcplaygroundpage/Contents.swift
+++ aoc15.playground/Pages/Day15.xcplaygroundpage/Contents.swift
@@ -3,6 +3,6 @@
 import Foundation
 
 part1()
-
+part2()
 
 //: [Next](@next)
--- aoc15.playground/Pages/Day15.xcplaygroundpage/Sources/Day15.swift
+++ aoc15.playground/Pages/Day15.xcplaygroundpage/Sources/Day15.swift
@@ -16,11 +16,16 @@ extension Ingredient {
     }
 }
 
-func cookieScoreFrom(_ ingredients: [(Int, Ingredient)]) -> Int {
+func cookieScoreFrom(_ ingredients: [(Int, Ingredient)], withCalories: Int? = nil) -> Int {
     let capacity = ingredients.map{ $0 * $1.capacity }.reduce(0, +)
     let durability = ingredients.map{ $0 * $1.durability }.reduce(0, +)
     let flavor = ingredients.map{ $0 * $1.flavor }.reduce(0, +)
     let texture = ingredients.map{ $0 * $1.texture }.reduce(0, +)
+    let calories = ingredients.map{ $0 * $1.calories }.reduce(0, +)
+
+    if let withCalories = withCalories {
+        guard calories == withCalories else { return 0 }
+    }
     
     return max(0, capacity) * max(0, durability) * max(0, flavor) * max(0, texture)
 }
@@ -37,6 +42,26 @@ public func part1() -> Int {
                 (fro, Ingredient(capacity: 0, durability: -1, flavor: 4, texture: 0, calories: 6)),
                 (sug, Ingredient(capacity: -1, durability: 0, flavor: 0, texture: 2, calories: 8)),
                 ])
+                best = max(score, best)
+            }
+        }
+    }
+    
+    return best
+}
+
+public func part2() -> Int {
+    var best = 0
+    for spr in 0...100 {
+        for pea in 0...(100-spr) {
+            for fro in 0...(100-spr-pea) {
+                let sug = 100 - spr - pea - fro
+                let score = cookieScoreFrom([
+                    (spr, Ingredient(capacity: 5, durability: -1, flavor: 0, texture: 0, calories: 5)),
+                    (pea, Ingredient(capacity: -1, durability: 3, flavor: 0, texture: 0, calories: 1)),
+                    (fro, Ingredient(capacity: 0, durability: -1, flavor: 4, texture: 0, calories: 6)),
+                    (sug, Ingredient(capacity: -1, durability: 0, flavor: 0, texture: 2, calories: 8)),
+                ], withCalories: 500)
                 best = max(score, best)
             }
         }
