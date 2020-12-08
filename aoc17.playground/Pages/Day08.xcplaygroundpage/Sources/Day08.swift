9a3309e717c5128f16fbebd4c83799ad6298e33d
Lachlan Roche <hello@lachlan.roche.name>
2020-12-08 21:18:03 +1000

    2017 day08 part1

---
+++ aoc17.playground/Pages/Day08.xcplaygroundpage/Sources/Day08.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+
+public func part1() -> Int {
+    var register = Dictionary<String, Int>()
+    
+    func compare(_ op: String, _ a: Int, _ b: Int) -> Bool {
+        if op == "==" {
+            return a == b
+        } else if op == "!=" {
+            return a != b
+        } else if op == ">" {
+            return a > b
+        } else if op == ">=" {
+            return a >= b
+        } else if op == "<=" {
+            return a <= b
+        } else if op == "<" {
+            return a < b
+        }
+        return false
+    }
+    func accumulate(_ op: String, _ a: Int, _ b: Int) -> Int {
+        if op == "inc" {
+            return a + b
+        } else {
+            return a - b
+        }
+    }
+    
+    stringsFromFile(named: "input")
+        .filter{ $0 != "" }
+        .forEach { str in
+            let s = str.components(separatedBy: " ")
+            let test_reg = register[s[4]] ?? 0
+            let test_val = Int(s[6])!
+            if compare(s[5], test_reg, test_val) {
+                let reg_val = register[s[0]] ?? 0
+                let val = Int(s[2])!
+                let result = accumulate(s[1], reg_val, val)
+                register[s[0]] = result
+            }
+        }
+    
+    return register.map{ $0.value }.reduce(0, max)
+}
