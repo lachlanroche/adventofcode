9018d3f8d3c363100bedc802f9ea317b4a87513b
Lachlan Roche <hello@lachlan.roche.name>
2020-12-08 17:29:49 +1000

    2020 day8 part2

--- aoc20/src/aoc/day08.clj
+++ aoc20/src/aoc/day08.clj
@@ -29,6 +29,47 @@
           (= op "jmp")
           (recur (conj seen pc) (+ pc val) acc))))))
 
+(defn run-p2
+  [prog target]
+  (loop [seen #{} pc 0 acc 0]
+    (let [inst (get prog pc)
+          op (nth inst 0)
+          val (nth inst 1)]
+      (cond
+        (seen pc)
+        nil
+        (= target pc)
+        acc
+        (nil? inst)
+        nil
+        (= op "acc")
+        (recur (conj seen pc) (inc pc) (+ acc val))
+        (= op "nop")
+        (recur (conj seen pc) (inc pc) acc)
+        (= op "jmp")
+        (recur (conj seen pc) (+ pc val) acc)))))
+
+(defn run-with-subst-p2
+  [prog i]
+  (let [op (first (nth prog i))
+        val (second (nth prog i))
+        po (if (= "jmp" op) "nop" "jmp")
+        prog (vec (assoc prog i [po val]))]
+    (run-p2 prog (count prog))))
+
+(defn part2
+  []
+  (let [prog (vec (input-lines))]
+    (loop [i 0]
+      (let [op (first (nth prog i))]
+        (if (= op "acc")
+          (recur (inc i))
+          (let [acc (run-with-subst-p2 prog i)]
+            (if (nil? acc)
+              (recur (inc i))
+              acc)))))))
+
 (comment
   (part1)
+  (part2)
   )
