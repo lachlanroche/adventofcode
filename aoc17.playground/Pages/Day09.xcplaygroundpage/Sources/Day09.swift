318203c48cc0cf7811b160ed1bd3efb0bf1afc6d
Lachlan Roche <hello@lachlan.roche.name>
2020-12-10 20:45:58 +1000

    2017 day09 part1

---
+++ aoc17.playground/Pages/Day09.xcplaygroundpage/Sources/Day09.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+
+public func part1() -> Int {
+    var depth = 0
+    var score = 0
+    var skip = false
+    var garbage = false
+    for c in stringsFromFile(named: "input")[0] {
+        if skip {
+            skip = false
+
+        } else if c == "!" {
+            skip = true
+
+        } else if garbage {
+            if c == ">" {
+                garbage = false
+            }
+            
+        } else {
+            if c == "<" {
+                garbage = true
+                
+            } else if c == "{" {
+                depth += 1
+                score += depth
+
+            } else if c == "}" {
+                depth -= 1
+            }
+        }
+    }
+    
+    return score
+}
