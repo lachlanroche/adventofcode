337d3ecacffc872bbaf26c4d9442812a92737334
Lachlan Roche <hello@lachlan.roche.name>
2020-12-10 21:08:08 +1000

    2017 day11 part1

---
+++ aoc17.playground/Pages/Day11.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+part1()
+part2()
+
+//: [Next](@next)
---
+++ aoc17.playground/Pages/Day11.xcplaygroundpage/Sources/Day11.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+extension Point {
+    mutating func n() {
+        y += 2
+    }
+    mutating func nw() {
+        x += 1
+        y += 1
+    }
+    mutating func ne() {
+        x -= 1
+        y += 1
+    }
+    mutating func s() {
+        y -= 2
+    }
+    mutating func sw() {
+        x += 1
+        y -= 1
+    }
+    mutating func se() {
+        x -= 1
+        y -= 1
+    }
+    
+}
+
+func walk() -> [Point] {
+    var path = [Point]()
+    var point = Point(x:0, y:0)
+    for step in stringsFromFile(named: "input")[0].components(separatedBy: ",") {
+        switch step {
+            case "n":
+                point.n()
+                break
+            case "nw":
+                point.nw()
+                break
+            case "ne":
+                point.ne()
+            break
+            case "s":
+                point.s()
+                break
+            case "sw":
+                point.sw()
+                break
+            case "se":
+                point.se()
+                break
+            default:
+            break
+        }
+        path.append(point)
+    }
+    return path
+}
+
+public func part1() -> Int {
+    let point = walk().last!
+    return (abs(point.x) + abs(point.y)) / 2
+}
