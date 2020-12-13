54a5403651bf1a2d450e98796caf3f0dffbd55b7
Lachlan Roche <hello@lachlan.roche.name>
2020-12-13 14:41:24 +1000

    2017 day 16 part2

--- aoc17.playground/Pages/Day16.xcplaygroundpage/Sources/Day16.swift
+++ aoc17.playground/Pages/Day16.xcplaygroundpage/Sources/Day16.swift
@@ -24,3 +24,39 @@ public func part1() -> String {
     }
     return String(arr)
 }
+
+public func part2() -> String {
+    var arr = Array("ociedpjbmfnkhlga")
+    let instr = stringsFromFile(named: "input")[0].components(separatedBy: ",")
+    var seen = Set<String>()
+    var history = Dictionary<Int, String>()
+    for i in 0... {
+        for s in instr {
+            let t = s.dropFirst()
+            let tt = t.components(separatedBy: "/")
+            if s.hasPrefix("s") {
+                let sn = arr.count - Int(s.dropFirst())!
+                let arr0 = arr
+                arr = []
+                arr.append(contentsOf: arr0.dropFirst(sn))
+                arr.append(contentsOf: arr0.prefix(upTo: sn))
+            } else if s.hasPrefix("x") {
+                let x0 = Int(tt[0])!
+                let x1 = Int(tt[1])!
+                arr.swapAt(x0, x1)
+                
+            } else if s.hasPrefix("p") {
+                let p0 = arr.firstIndex(of: tt[0].first!)!
+                let p1 = arr.firstIndex(of: tt[1].first!)!
+                arr.swapAt(p0, p1)
+            }
+        }
+        let hash = String(arr)
+        if seen.contains(hash) {
+            return String(history[1_000_000_000 % i]!)
+        }
+        seen.insert(hash)
+        history[i] = hash
+    }
+    return ""
+}
