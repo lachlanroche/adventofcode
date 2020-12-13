32fd904f318b07b4f873552c38449b86c5e7b2a0
Lachlan Roche <hello@lachlan.roche.name>
2020-12-13 17:40:50 +1000

    2020 day 13 part1

---
+++ aoc20/src/aoc/day13.clj
@@ -0,0 +1,34 @@
+(ns aoc20.day13
+  (:require [clojure.math.combinatorics :as combo]))
+
+(defn input-data
+  []
+  (let [s (->> "aoc/day13.txt"
+               clojure.java.io/resource
+               slurp
+               clojure.string/split-lines
+               )
+        earliest (Integer. (first s))
+        buses (->>
+               (clojure.string/split (second s) #",")
+               (filter #(not= "x" %))
+               (map #(Integer. %)))
+        buses2 (->>
+                (clojure.string/split (second s) #",")
+                (map-indexed #(vector %1 %2))
+                (filter #(not= "x" (second %)))
+                (map #(vector (first %) (Integer. (second %)))))
+        ]
+    {:earliest earliest :buses buses :buses2 buses2}))
+
+(defn part1
+  []
+  (let [input (input-data)
+        earliest (:earliest input)
+        buses (:buses input)]
+    (loop [i earliest]
+      (let [bus (some #(if (= 0 (mod i %)) % nil) buses)]
+        (if (nil? bus)
+          (recur (inc i))
+          (* bus (- i earliest)))))))
+
