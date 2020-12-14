843c8af2417de09542701a67813e87b4c520bf2a
Lachlan Roche <hello@lachlan.roche.name>
2020-12-14 22:40:00 +1000

    2020 day 14 part2

--- aoc20/src/aoc/day14.clj
+++ aoc20/src/aoc/day14.clj
@@ -46,3 +46,62 @@
         (let [val (apply-mask mask (Integer. c))
               mem (assoc mem b val)]
         (recur mem mask lines))))))
+
+(defn make-floating-mask
+  [bits]
+  (loop [fns [identity] bits bits]
+    (let [bit (first bits)
+          bits (rest bits)]
+      (cond
+        (nil? bit)
+        fns
+        :else
+        (let [f0 (map #(comp (fn [v] (bit-clear v bit)) %) fns)
+              f1 (map #(comp (fn [v] (bit-set v bit)) %) fns)]
+          (recur (concat f0 f1) bits))))))
+
+(defn make-fixed-mask
+  [bits]
+  (loop [f identity bits bits]
+    (let [bit (first bits)
+          bits (rest bits)]
+      (cond
+        (nil? bit)
+        f
+        :else
+        (let [f (comp (fn [v] (bit-set v bit)) f)]
+          (recur f bits))))))
+
+(defn addr-mask-fns
+  [m]
+  (let [bit-map (->> m
+                     (map-indexed #(vector (- 35 %1) %2))
+                     (group-by second))
+        fn-1 (->> (get bit-map \1)
+                  (map first)
+                  make-fixed-mask)
+        fn-X (->> (get bit-map \X)
+                  (map first)
+                  make-floating-mask)
+        fn-1X (map #(comp fn-1 %) fn-X)
+        ]
+    fn-1X))
+
+(defn part2
+  []
+  (loop [mem {} mask identity lines (input-lines)]
+    (let [line (first lines)
+          lines (rest lines)
+          [a b c] line]
+      (cond
+        (nil? line)
+        (reduce + 0 (vals mem))
+        (= "mask" a)
+        (recur mem (addr-mask-fns b) lines)
+        (= "mem" a)
+        (let [val (Integer. c)
+              b (Integer. b)
+              bs (map #(% b) mask)
+              mem (reduce (fn [mem b] (assoc mem b val)) mem bs)
+              ]
+        (recur mem mask lines))))))
