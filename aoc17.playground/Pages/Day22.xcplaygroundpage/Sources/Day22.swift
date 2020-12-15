aec2a26774e67c78a8f50080b06351d9e13d5fb6
Lachlan Roche <hello@lachlan.roche.name>
2020-12-15 13:07:32 +1000

    2017 day 22 part1

---
+++ aoc17.playground/Pages/Day22.xcplaygroundpage/Sources/Day22.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+func importData() -> Dictionary<Point, Character> {
+    var result = Dictionary<Point, Character>()
+    var x = -12
+    var y = -12
+    for s in stringsFromFile(named: "input") {
+        if s == "" {
+            break
+        }
+        for c in s {
+            if c == "#" {
+                result[Point(x: x, y: y)] = c
+            }
+            x += 1
+        }
+        y += 1
+        x = -12
+    }
+    
+    return result
+}
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+enum Direction {
+    case down
+    case up
+    case left
+    case right
+}
+
+extension Direction {
+    func turnLeft() -> Direction {
+        switch self {
+            case .up: return .left
+            case .left: return .down
+            case .down: return .right
+            case .right: return .up
+        }
+    }
+    func turnRight() -> Direction {
+        switch self {
+            case .up: return .right
+            case .right: return .down
+            case .down: return .left
+            case .left: return .up
+        }
+    }
+}
+
+extension Point {
+    func step(_ direction: Direction) -> Point {
+        switch direction {
+            case .down:
+                return Point(x: x, y: y + 1)
+            case .up:
+                return Point(x: x, y: y - 1)
+            case .right:
+                return Point(x: x + 1, y: y)
+            case .left:
+                return Point(x: x - 1, y: y)
+        }
+    }
+}
+
+public func part1() -> Int {
+    var world = importData()
+    var position = Point(x: 0, y: 0)
+    var direction = Direction.up
+    var infections = 0
+    
+    for _ in 1..<10000 {
+        
+        if let _ = world[position] {
+            direction = direction.turnRight()
+            world.removeValue(forKey: position)
+        } else {
+            direction = direction.turnLeft()
+            world[position] = "#"
+            infections += 1
+        }
+        position = position.step(direction)
+    }
+    
+    return infections
+}
