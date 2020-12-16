a3d8a2353df2a638b5aa21f3bd8784e78f71a6e4
Lachlan Roche <hello@lachlan.roche.name>
2020-12-17 09:11:31 +1000

    2017 day 17 part 2

--- aoc17.playground/Pages/Day17.xcplaygroundpage/Sources/Day17.swift
+++ aoc17.playground/Pages/Day17.xcplaygroundpage/Sources/Day17.swift
@@ -11,3 +11,18 @@ public func part1() -> Int {
     }
     return arr[(pos + 1) % arr.count]
 }
+
+public func part2() -> Int {
+    var arr = Array(repeating: 0, count: 50_000_001)
+    var len = 1
+    var pos = 0
+    let step = 337
+    for i in 1...50_000_000 {
+        pos = (pos + step) % len
+        arr[pos + 1] = i
+        len += 1
+        pos = pos + 1
+    }
+    
+    return arr[1]
+}
