91be2e2f96c738efa6f595e71777b3608dbe672e
Lachlan Roche <hello@lachlan.roche.name>
2020-12-16 11:09:20 +1000

    2017 day 03 refactor

--- aoc17.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
+++ aoc17.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
@@ -7,35 +7,43 @@ struct Map {
     var step = 1
     var distance = 1
     mutating func right() {
-        x += step
-        distance += step
+        x += 1
+        distance += 1
     }
     mutating func up() {
-        y += step
-        distance += step
+        y += 1
+        distance += 1
     }
     mutating func left() {
-        x -= step
-        distance += step
+        x -= 1
+        distance += 1
     }
     mutating func down() {
-        y -= step
-        distance += step
+        y -= 1
+        distance += 1
     }
 }
 
 extension Map {
     mutating func walk(to target: Int) {
         while true {
-            right()
-            guard distance < target else { x -= distance - target; break }
-            up()
-            guard distance < target else { y -= distance - target; break }
+            for _ in 0..<step {
+                right()
+                guard distance != target else { return }
+            }
+            for _ in 0..<step {
+                up()
+                guard distance != target else { return }
+            }
             step += 1
-            left()
-            guard distance < target else { x += distance - target; break }
-            down()
-            guard distance < target else { y += distance - target; break }
+            for _ in 0..<step {
+                left()
+                guard distance != target else { return }
+            }
+            for _ in 0..<step {
+                down()
+                guard distance != target else { return }
+            }
             step += 1
         }
     }
@@ -46,5 +54,3 @@ public func part1() -> Int {
     map.walk(to: 277678)
     return abs(map.x) + abs(map.y)
 }
-
-//: [Next](@next)
