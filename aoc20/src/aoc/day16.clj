ff1b878f2d20bf195638722e7d3db18c5d026c33
Lachlan Roche <hello@lachlan.roche.name>
2020-12-16 18:20:11 +1000

    2020 day 16 part 2

--- aoc20/src/aoc/day16.clj
+++ aoc20/src/aoc/day16.clj
@@ -80,3 +80,57 @@
     (->> nums
          (filter #(not (valid-for-something? %)))
          (reduce + 0))))
+
+(defn ticket-looks-valid?
+  [nums]
+  (reduce (fn [result n] (and result (valid-for-something? n))) true nums))
+
+(defn slot-match-rules
+  [rules n]
+  (->> rules
+       (map (fn [[k f]] (if (f n) #{k} #{})))
+       (apply clojure.set/union)))
+
+(defn match-rules
+  [rules nums]
+  (->> nums
+       (map #(slot-match-rules rules %))))
+
+(defn find-columns
+  [candidates]
+  (loop [result {} singles [] candidates candidates]
+    (cond
+      (empty? candidates)
+      result
+      (empty? singles)
+      (let [singles (filter #(= 1 (count (val %))) candidates)]
+        (recur result singles candidates))
+      :else
+      (let [single (first singles)
+            sk (key single)
+            sv (first (val single))
+            result (assoc result sv sk)
+            candidates (dissoc candidates sk)
+            candidates (into {} (for [[k v] candidates] [k (disj v sv)]))]
+        (recur result (rest singles) candidates)))))
+
+(defn part2
+  []
+  (let [tickets (->> (tickets)
+                     (map (fn [ts] (map #(Integer. %) ts))))
+        mine (first tickets)
+        tickets (->> tickets
+                     rest
+                     (filter ticket-looks-valid?))
+        rules (rules)
+        candidates (->> tickets
+                        (map #(match-rules rules %))
+                        (apply map vector)
+                        (map #(apply clojure.set/intersection %))
+                        (into {} (map-indexed #(hash-map %1 %2))))
+        column-names (find-columns candidates)
+        column-indexes (-> column-names
+                           (select-keys  [:departure-location :departure-station :departure-platform :departure-track :departure-date :departure-time])
+                           vals)
+        ]
+    (reduce (fn [acc n] (* acc (nth mine n))) 1 column-indexes)))
