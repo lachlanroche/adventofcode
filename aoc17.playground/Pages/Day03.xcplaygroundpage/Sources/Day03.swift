c519ba67558d96f05d9e9b63c9ad27d7d673a8b9
Lachlan Roche <hello@lachlan.roche.name>
2020-12-16 11:35:37 +1000

    2017 day 03 part 2

--- aoc17.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
+++ aoc17.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
@@ -54,3 +54,61 @@ public func part1() -> Int {
     map.walk(to: 277678)
     return abs(map.x) + abs(map.y)
 }
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+public func part2() -> Int {
+    var map = Map()
+    var values = Dictionary<Point, Int>()
+    values[Point(x: 0, y: 0)] = 1
+    var result = 1
+    let target = 277678
+    
+    func visit(x: Int, y: Int) {
+        if x == 0 && y == 0 {
+            return
+        }
+
+        var val = 0
+        val += values[Point(x: x + 1, y: y + 1)] ?? 0
+        val += values[Point(x: x,     y: y + 1)] ?? 0
+        val += values[Point(x: x - 1, y: y + 1)] ?? 0
+        val += values[Point(x: x + 1, y: y)] ?? 0
+        val += values[Point(x: x - 1, y: y)] ?? 0
+        val += values[Point(x: x + 1, y: y - 1)] ?? 0
+        val += values[Point(x: x,     y: y - 1)] ?? 0
+        val += values[Point(x: x - 1, y: y - 1)] ?? 0
+        values[Point(x: x, y: y)] = val
+        result = val
+    }
+
+    while true {
+        for _ in 0..<map.step {
+            map.right()
+            visit(x: map.x, y: map.y)
+            guard result <= target else { return result }
+        }
+        for _ in 0..<map.step {
+            map.up()
+            visit(x: map.x, y: map.y)
+            guard result <= target else { return result }
+        }
+        map.step += 1
+        for _ in 0..<map.step {
+            map.left()
+            visit(x: map.x, y: map.y)
+            guard result <= target else { return result }
+        }
+        for _ in 0..<map.step {
+            map.down()
+            visit(x: map.x, y: map.y)
+            guard result <= target else { return result }
+        }
+        map.step += 1
+    }
+    
+    return 0
+}
