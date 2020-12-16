5364545a3280a6594adb98715e6b93975d281a8d
Lachlan Roche <hello@lachlan.roche.name>
2020-12-17 08:49:32 +1000

    2017 day 14 part 2

--- aoc17.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
+++ aoc17.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
@@ -76,3 +76,64 @@ public func part1() -> Int {
     }
     return result
 }
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    
+    func neighbors() -> [Point] {
+        return [
+            Point(x: x,     y: y + 1),
+            Point(x: x,     y: y - 1),
+            Point(x: x + 1, y: y),
+            Point(x: x - 1, y: y),
+        ]
+    }
+}
+
+struct World {
+    var map = Dictionary<Point, Int>()
+    
+    mutating func paint(_ xy: Point, with color: Int) {
+        var seen = Set<Point>()
+        var work = Set<Point>()
+        work.insert(xy)
+        
+        while work.count != 0 {
+            let p = work.removeFirst()
+            map[p] = color
+            seen.insert(p)
+            for n in p.neighbors() {
+                guard let _ = map[n] else { continue }
+                guard !seen.contains(n) else { continue }
+                work.insert(n)
+            }
+        }
+    }
+}
+
+public func part2() -> Int {
+    let input = "hwlqcszp"
+    var world = World()
+    for y in 0..<128 {
+        let str = knothash(of: "\(input)-\(y)")
+            .map{ $0.hashDescription  }
+            .joined()
+        var x = 0
+        for c in str {
+            if c == "#" {
+                world.map[Point(x: x, y: y)] = 0
+            }
+            x += 1
+        }
+    }
+    
+    var i = 1
+    for k in world.map.keys {
+        guard world.map[k] == 0 else { continue }
+        world.paint(k, with: i)
+        i += 1
+    }
+
+    return Set(world.map.values).count
+}
