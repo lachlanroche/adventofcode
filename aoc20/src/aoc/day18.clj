6b90b5f52d4383c132f6a4ce133a83904d5f62e6
Lachlan Roche <hello@lachlan.roche.name>
2020-12-19 22:17:57 +1000

    2020 day 18 switch to instaparse

--- aoc20/src/aoc/day18.clj
+++ aoc20/src/aoc/day18.clj
@@ -1,5 +1,5 @@
 (ns aoc20.day13
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [instaparse.core :as insta]))
 
 (defn input-data
   []
@@ -7,45 +7,28 @@
                clojure.java.io/resource
                slurp
                clojure.string/split-lines
-               (map #(clojure.string/split % #" "))
+               #_(map #(clojure.string/split % #" "))
                )]
     s))
 
-(defn evil
-  [str]
-  (loop [acc [0] ops [+] str str]
-    (let [s (first str)
-          str (rest str)]
-      (tap> [s acc ops])
-      (cond
-        (nil? s)
-        (first acc)
-        (= "+" s)
-        (recur acc (concat [+] ops) str)
-        (= "*" s)
-        (recur acc (concat [*] ops) str)
-        (= "(" s)
-        (recur (concat [0] acc) (concat [+] ops) str)
-        (= ")" s)
-        (let [val0 (first acc)
-              val1 (second acc)
-              acc (rest acc)
-              op (first ops)
-              ops (rest ops)
-              val (op val0 val1)
-              acc (assoc (vec acc) 0 val)]
-          (recur acc ops str))
-        (number? (read-string s))
-        (let [op (first ops)
-              ops (rest ops)
-              val (first acc)
-              val (op val (read-string s))
-              acc (assoc (vec acc) 0 val)]
-          (recur acc ops str))))))
+(def arithmetic
+  (insta/parser
+    "expr = add-mul
+     <add-mul> = term | add | mul
+     add = add-mul <'+'> term
+     mul = add-mul <'*'> term
+     <term> = number | <'('> add-mul <')'>
+     number = #'[0-9]+'"
+    :auto-whitespace :standard))
 
 (defn part1
-       []
-  (->> (input-data)
-       (map evil)
-       (reduce + 0)))
+  []
+  (reduce
+   (fn [acc expr]
+     (+ acc
+        (->> (arithmetic expr)
+             (insta/transform parse-tree->sexp))))
+   0
+   (input-data)))
+
 
