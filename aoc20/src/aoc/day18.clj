d005278396ddeb96c3dd8d328772d69409c06e42
Lachlan Roche <hello@lachlan.roche.name>
2020-12-19 22:18:08 +1000

    2020 day 18 part 2

--- aoc20/src/aoc/day18.clj
+++ aoc20/src/aoc/day18.clj
@@ -31,4 +31,23 @@
    0
    (input-data)))
 
+(def arithmetic2
+  (insta/parser
+    "expr = add-expr | mul-expr
+     <add-expr> = term | add
+     add = add-expr <'+'> term
+     <mul-expr> = add-expr | mul
+     mul = mul-expr <'*'> add-expr
+     <term> = number | <'('> mul-expr <')'>
+     number = #'[0-9]+'"
+    :auto-whitespace :standard))
 
+(defn part2
+  []
+  (reduce
+   (fn [acc expr]
+     (+ acc
+        (->> (arithmetic2 expr)
+             (insta/transform parse-tree->sexp))))
+   0
+   (input-data)))
