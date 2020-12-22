600c463c008a3997e2b0cef9479b8998c8ee4cbd
Lachlan Roche <hello@lachlan.roche.name>
2020-12-22 17:05:58 +1000

    2020 day 22 part 1

---
+++ aoc20/src/aoc/day22.clj
@@ -0,0 +1,57 @@
+(ns aoc20.day22)
+
+(defn input-file
+  []
+  (let [lines (->> (str "aoc/day22.txt")
+                   clojure.java.io/resource
+                   slurp
+                   clojure.string/split-lines
+                   (map #(clojure.string/split % #"[ :]"))
+                   )]
+    (loop [decks [] deck [] lines lines]
+      (let [line (first lines)
+            lines (rest lines)]
+        (cond
+          (nil? line)
+          (conj decks deck)
+          (= "Player" (first line))
+          (recur decks [] lines)
+          (= "" (first line))
+          (recur (conj decks deck) [] lines)
+          :else
+          (recur decks (conj deck (Integer. (first line))) lines))))
+    ))
+
+(defn play-step
+  [aa bb]
+  (let [a (first aa)
+        aa (rest aa)
+        b (first bb)
+        bb (rest bb)]
+  (if (> a b)
+    [(concat aa [a b]) bb]
+    [aa (concat bb [b a])])))
+
+(defn score
+  [aa]
+  (->> aa
+       reverse
+       (map-indexed #(* (inc %1) %2))
+       (reduce +)))
+
+(defn play-all
+  [[aa bb]]
+  (loop [aa aa bb bb]
+    (cond
+      (= 0 (count aa))
+      (score bb)
+      (= 0 (count bb))
+      (score aa)
+      :else
+      (let [[aa bb] (play-step aa bb)]
+        (recur aa bb)))))
+
+(defn part1
+  []
+  (play-all (input-file)))
+
