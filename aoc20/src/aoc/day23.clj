994dfb0da669cc2a380f24119716f59cb9e52df1
Lachlan Roche <hello@lachlan.roche.name>
2020-12-24 18:55:58 +1000

    2020 day 23 part 2

--- aoc20/src/aoc/day23.clj
+++ aoc20/src/aoc/day23.clj
@@ -35,3 +35,39 @@
                           (drop (inc dest-idx) deck-tail)
                           [current])))))))
 
+(defn load-deck-map
+  []
+  (let [cups (concat (input-data) (range 10 1000001))
+        c (first cups)]
+    (loop [c c
+           deck {(last cups) c}
+           cups cups]
+      (let [d (first cups)
+            cups (rest cups)]
+        (if (nil? d)
+          deck
+          (recur d (assoc deck c d) cups))))))
+
+(defn part2
+  []
+  (loop [i 0
+         current (first (input-data))
+         deck (load-deck-map)]
+    (let [three [(deck current)]
+          three (conj three (deck (last three)))
+          three (conj three (deck (last three)))
+          deck (assoc deck current (deck (last three)))
+          dest (loop [b (dec current)]
+                 (let [b (if (= 0 b) 1000000 b)]
+                   (if (nil? (some #{b} three))
+                     b
+                     (recur (dec b)))))
+          after (deck dest)
+          deck (assoc deck
+                      dest (first three)
+                      (first three) (second three)
+                      (second three) (last three)
+                      (last three) after)]
+      (if (= 10000000 i)
+        (* (deck 1) (deck (deck 1)))
+        (recur (inc i) (deck current) deck)))))
