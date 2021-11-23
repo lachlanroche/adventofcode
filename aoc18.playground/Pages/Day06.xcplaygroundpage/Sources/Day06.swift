257278b57139a03d1628e8121f9b693c71fd376d
Lachlan Roche <hello@lachlan.roche.name>
2021-11-23 19:17:17 +1000

    2018 day 06 part 1

---
+++ aoc18.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    init?( str: String ) {
+        guard str != "" else { return nil }
+        let s = str.components(separatedBy: ",")
+        x = Int(s[0].trimmingCharacters(in: .whitespacesAndNewlines))!
+        y = Int(s[1].trimmingCharacters(in: .whitespacesAndNewlines))!
+    }
+}
+
+extension Point {
+    func manhattan(_ p: Point) -> Int {
+        return abs(x - p.x) + abs(y - p.y)
+    }
+    
+    func closest(_ points: [Point]) -> Point? {
+        var pd = 1_000_000
+        var pt: Point? = nil
+        for p in points {
+            let d = manhattan(p)
+            if d < pd {
+                pt = p
+                pd = d
+            } else if d == pd {
+                pt = nil
+            }
+        }
+        return pt
+    }
+}
+
+public func part1() -> Int {
+    let points = stringsFromFile().compactMap( Point.init )
+
+    var edgePoints = Set<Point>()
+    var closest = Dictionary<Point, Int>()
+    for pt in points {
+        closest[pt] = 0
+    }
+ 
+    for x in -1000...1000 {
+        for y in -1000...1000 {
+            guard let p = Point(x: x, y: y).closest(points) else { continue }
+
+            if abs(x) == 1000 || abs(y) == 1000 {
+                edgePoints.insert(p)
+            }
+            closest[p] = 1 + closest[p]!
+        }
+    }
+    
+    for ep in edgePoints {
+        closest.removeValue(forKey: ep)
+    }
+
+    return closest.values.max() ?? 0
+}
