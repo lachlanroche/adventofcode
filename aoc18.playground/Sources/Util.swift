7cb4c20b5ec093fd188a4c43cba28969cb7dbded
Lachlan Roche <hello@lachlan.roche.name>
2021-11-25 21:52:16 +1000

    2018 day 08 part 1

--- aoc18.playground/Sources/Util.swift
+++ aoc18.playground/Sources/Util.swift
@@ -1,12 +1,17 @@
 import Foundation
 
+public func stringFromFile(named: String = "input") -> String
+{
+    guard let path = Bundle.main.path(forResource: named, ofType: "txt") else { return "" }
+    guard let data = FileManager.default.contents(atPath: path) else { return "" }
+    guard let content = String(data:data, encoding:String.Encoding.utf8) else { return "" }
+    
+    return content
+}
+
 public func stringsFromFile(named: String = "input") -> [String]
 {
-    guard let path = Bundle.main.path(forResource: named, ofType: "txt") else { return [] }
-    guard let data = FileManager.default.contents(atPath: path) else { return [] }
-    guard let content = String(data:data, encoding:String.Encoding.utf8) else { return [] }
-    
-    return content.components(separatedBy: CharacterSet.newlines)
+    return stringFromFile(named: named).components(separatedBy: CharacterSet.newlines)
 }
 
 public func numbersFromFile(named: String) -> [Int]
@@ -15,13 +20,13 @@ public func numbersFromFile(named: String) -> [Int]
         .compactMap(Int.init)
 }
 
-public func stringsFromString(_ str: String) -> [String]
+public func stringsFromString(_ str: String, sep: String = ",") -> [String]
 {
-    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: ",")))
+    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: sep)))
 }
 
-public func numbersFromString(_ str: String) -> [Int]
+public func numbersFromString(_ str: String, sep: String = ",") -> [Int]
 {
-    return stringsFromString(str)
+    return stringsFromString(str, sep: sep)
         .compactMap(Int.init)
 }
