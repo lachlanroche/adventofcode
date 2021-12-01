d7d0aaf26f1e493ffdb2cdd14b9d55a392087082
Lachlan Roche <hello@lachlan.roche.name>
2021-12-01 21:27:29 +1000

    2021 day 01 part 2

--- aoc21.playground/Pages/Day01.xcplaygroundpage/Sources/Day01.swift
+++ aoc21.playground/Pages/Day01.xcplaygroundpage/Sources/Day01.swift
@@ -14,3 +14,15 @@ public func part1() -> Int {
     return count
 }
 
+public func part2() -> Int {
+    var prev = 1_000_000
+    var count = 0
+    for ds in data.windows(ofCount:3) {
+        let dss = ds.reduce(0, +)
+        if dss > prev {
+            count = 1 + count
+        }
+        prev = dss
+    }
+    return count
+}
