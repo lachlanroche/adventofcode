ae743b63cf4db9c21ee31e33c1060d38addd7b3d
Lachlan Roche <hello@lachlan.roche.name>
2021-12-03 14:29:26 +1000

    2021 day 02 part 2

--- aoc21.playground/Pages/Day02.xcplaygroundpage/Sources/Day02.swift
+++ aoc21.playground/Pages/Day02.xcplaygroundpage/Sources/Day02.swift
@@ -24,3 +24,29 @@ public func part1() -> Int {
     }
     return z*x
 }
+
+public func part2() -> Int {
+    var z = 0
+    var x = 0
+    var aim = 0
+    for s in stringsFromFile(named: "input") {
+        let ss = stringsFromString(s, sep: " ")
+        guard ss.count == 2 else { continue }
+        let n = Int(ss[1])!
+        switch ss[0] {
+        case "up":
+            aim = aim - n
+            break
+        case "down":
+            aim = aim + n
+            break
+        case "forward":
+            x = x + n
+            z = z + (aim * n)
+            break
+        default:
+            break
+        }
+    }
+    return z*x
+}
