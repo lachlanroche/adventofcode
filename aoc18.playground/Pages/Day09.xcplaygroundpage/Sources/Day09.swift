d3ee3aa4f4c64d1d9f7608af0e9f9f7ee2343278
Lachlan Roche <hello@lachlan.roche.name>
2021-12-05 18:29:27 +1000

    2018 day 09 part 1

---
+++ aoc18.playground/Pages/Day09.xcplaygroundpage/Sources/Day09.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+let ELF = 468
+let MARBLE = 71843
+
+public func part1() -> Int {
+    var scores = Dictionary<Int,Int>()
+    var circle = [0]
+    var current = 1
+    var elf = 1
+    
+    for m in 1...MARBLE {
+        if 0 == m % 23 {
+            var pos = (current + circle.count - 7) % circle.count
+            if pos == circle.count - 1 {
+                current = 0
+            } else {
+                current = pos
+            }
+            let mr = circle.remove(at: pos)
+            scores[elf] = m + mr + (scores[elf] ?? 0)
+            
+        } else {
+            var pos = current
+            pos = (current + 1) % circle.count
+            current = pos + 1
+            circle.insert(m, at: current)
+        }
+        
+        elf = (1 + elf) % ELF
+    }
+    
+    return scores.values.max()!
+}
+
+public func part2() -> Int {
+    return 0
+}
