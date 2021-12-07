d8044153987a0db17dc0400a2fc8adcf0ec48a1b
Lachlan Roche <hello@lachlan.roche.name>
2021-12-08 03:45:43 +1000

    2018 day 11 part 2

--- aoc18.playground/Pages/Day11.xcplaygroundpage/Sources/Day11.swift
+++ aoc18.playground/Pages/Day11.xcplaygroundpage/Sources/Day11.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-typealias Point = (x: Int, y: Int)
-
 public func part1() -> (x: Int, y: Int) {
     let serial = 6548
     var result = (x: 0, y: 0)
@@ -29,6 +27,41 @@ public func part1() -> (x: Int, y: Int) {
             if watts > maxWatts {
                 maxWatts = watts
                 result = p
+            }
+        }
+    }
+    
+    return result
+}
+
+public func part2() -> (x: Int, y: Int, w: Int) {
+    let serial = 6548
+    var psum = Array(repeating: Array(repeating: 0, count: 301), count: 301)
+    
+    for cy in 1...300 {
+        for cx in 1...300 {
+            let rackId = cx + 10
+            var cell = rackId * cy
+            cell = cell + serial
+            cell = cell * rackId
+            cell = cell / 100
+            cell = cell % 10
+            cell = cell - 5
+            psum[cy][cx] = cell + psum[cy - 1][cx] + psum[cy][cx - 1] - psum[cy - 1][cx - 1]
+        }
+    }
+    
+    var result = (x: 0, y: 0, w: 0)
+    var maxWatts = -Int.max
+    for w in 1...300 {
+        for y in w...300 {
+            for x in w...300 {
+                var watts = psum[y][x] - psum[y - w][x] - psum[y][x - w] + psum[y - w][x - w]
+                
+                if watts > maxWatts {
+                    maxWatts = watts
+                    result = (x: x - w + 1, y: y - w + 1, w: w)
+                }
             }
         }
     }
