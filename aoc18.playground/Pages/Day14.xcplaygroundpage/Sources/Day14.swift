200c6c78b8d1ede32d9240c26e3ccf0921407a8d
Lachlan Roche <hello@lachlan.roche.name>
2021-12-08 18:17:58 +1000

    2018 day 14 part 2

--- aoc18.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
+++ aoc18.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
@@ -20,3 +20,38 @@ public func part1() -> [Int] {
         }
     }
 }
+
+public func part2() -> Int {
+    var data = [3, 7]
+    var e0 = 0
+    var e1 = 1
+    let target = [8, 8, 0, 7, 5, 1]
+    var result = [3, 7]
+    
+    while true {
+        let r = data[e0] + data[e1]
+        if r >= 10 {
+            data.append(1)
+            result.append(1)
+        }
+        
+        while result.count > target.count {
+            result.remove(at: 0)
+        }
+        if result == target {
+            return data.count - target.count
+        }
+        
+        data.append(r % 10)
+        result.append(r % 10)
+        e0 = (e0 + 1 + data[e0]) % data.count
+        e1 = (e1 + 1 + data[e1]) % data.count
+        
+        while result.count > target.count {
+            result.remove(at: 0)
+        }
+        if result == target {
+            return data.count - target.count
+        }
+    }
+}
