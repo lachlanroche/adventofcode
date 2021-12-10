7e72d654976c2e3f85a9f5135c22f9c9ae443451
Lachlan Roche <hello@lachlan.roche.name>
2021-12-10 15:29:36 +1000

    2021 day 10 part 2

--- aoc21.playground/Pages/Day10.xcplaygroundpage/Sources/Day10.swift
+++ aoc21.playground/Pages/Day10.xcplaygroundpage/Sources/Day10.swift
@@ -32,3 +32,40 @@ public func part1() -> Int {
     }
     return result
 }
+
+public func part2() -> Int {
+    let match: Dictionary<Character, Character> = ["(": ")", "{": "}", "[": "]", "<": ">"]
+
+    func complete(_ line: String) -> String? {
+        var stack = Array<Character>()
+        for c in line {
+            if let _ = match[c] {
+                stack.append(c)
+            } else {
+                let d = stack.removeLast()
+                if c == match[d] {
+                    continue
+                }
+                return nil
+            }
+        }
+        return String(stack.reversed().map({match[$0]!}))
+    }
+
+    var scores = [Int]()
+    for line in inputData() {
+        guard let s = complete(line) else { continue }
+        var score = 0
+        for c in s {
+            score = 5 * score
+            if c == ")" { score = 1 + score }
+            if c == "]" { score = 2 + score }
+            if c == "}" { score = 3 + score }
+            if c == ">" { score = 4 + score }
+        }
+        scores.append(score)
+    }
+    
+    scores.sort()
+    return scores[(scores.count / 2)]
+}
