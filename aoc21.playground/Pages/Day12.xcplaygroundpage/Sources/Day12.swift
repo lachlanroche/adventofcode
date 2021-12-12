fc7aa5eb9e4b6625dc6baab0cacef2b13030a057
Lachlan Roche <hello@lachlan.roche.name>
2021-12-12 16:00:48 +1000

    2021 day 12 part 1

---
+++ aoc21.playground/Pages/Day12.xcplaygroundpage/Sources/Day12.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func inputData() -> Dictionary<String,Set<String>> {
+    var result = Dictionary<String,Set<String>>()
+    for s in stringsFromFile() {
+        guard s.contains("-") else { continue }
+        let parts = s.components(separatedBy: "-")
+        let a = parts[0]
+        let b = parts[1]
+        result[a] = (result[a] ?? [])
+        result[b] = (result[b] ?? [])
+        result[a]!.insert(b)
+        result[b]!.insert(a)
+    }
+    return result
+}
+
+func search(_ pos: String, edges: Dictionary<String,Set<String>>, visited: Set<String>) -> Int {
+    if pos == "end" {
+        return 1
+    }
+    
+    var count = 0
+    for next in edges[pos]! {
+        if next == next.lowercased() && next != "end" {
+            if !visited.contains(next) {
+                count = count + search(next, edges: edges, visited: visited.union([next]))
+            }
+        } else {
+            count = count + search(next, edges: edges, visited: visited)
+        }
+    }
+    return count
+}
+
+public func part1() -> Int {
+    let edges = inputData()
+    
+    return search("start", edges: edges, visited: ["start"])
+}
+
