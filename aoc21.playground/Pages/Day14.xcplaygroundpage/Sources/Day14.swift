087cbea6c4704690221e36345f0aeb7fe49a2c9d
Lachlan Roche <hello@lachlan.roche.name>
2021-12-14 21:15:29 +1000

    2021 day 14 part 2

--- aoc21.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
+++ aoc21.playground/Pages/Day14.xcplaygroundpage/Sources/Day14.swift
@@ -42,3 +42,43 @@ public func part1() -> Int {
     return counts.values.max()! - counts.values.min()!
 }
 
+func pairs(of s: String) -> [String:Int] {
+    var result = [String: Int]()
+    var b:Character? = nil
+    for c in s {
+        if let b = b {
+            let t = String([b, c])
+            result[t] = 1 + (result[t] ?? 0)
+        }
+        b = c
+    }
+    return result
+}
+
+public func part2() -> Int {
+    let rules = rules()
+    var pairs = pairs(of: "PBFNVFFPCPCPFPHKBONB")
+    print(pairs)
+    for _ in 0..<40 {
+        var newPairs = [String:Int]()
+        for pk in pairs.keys {
+            let pn = pairs[pk]!
+            let c = rules[pk]!.first!
+            let l = pk.first!
+            let r = pk.last!
+            let a = String([l, c])
+            let b = String([c, r])
+            newPairs[a] = pn + (newPairs[a] ?? 0)
+            newPairs[b] = pn + (newPairs[b] ?? 0)
+        }
+        pairs = newPairs
+    }
+    
+    var counts = Dictionary<Character,Int>()
+    counts["B"] = 1
+    for pk in pairs.keys {
+        let c = pk.first!
+        counts[c] = pairs[pk]! + (counts[c] ?? 0)
+    }
+    return counts.values.max()! - counts.values.min()!
+}
