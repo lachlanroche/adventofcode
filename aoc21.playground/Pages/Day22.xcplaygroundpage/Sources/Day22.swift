46034d883a0d5fd70e7aceca9b0cc8468875c0f2
Lachlan Roche <hello@lachlan.roche.name>
2021-12-23 13:53:15 +1000

    2021 day 22 part 2

--- aoc21.playground/Pages/Day22.xcplaygroundpage/Sources/Day22.swift
+++ aoc21.playground/Pages/Day22.xcplaygroundpage/Sources/Day22.swift
@@ -31,6 +31,53 @@ struct Cube {
     let y1: Int
     let z0: Int
     let z1: Int
+
+    var volume: Int {
+        get { (x1 - x0) * (y1 - y0) * (z1 - z0) }
+    }
+
+    func contains(cube other: Cube) -> Bool {
+        return x0 <= other.x0 && x1 >= other.x1 &&
+        y0 <= other.y0 && y1 >= other.y1 &&
+        z0 <= other.z0 && z1 >= other.z1
+    }
+    
+    func intersects(cube other: Cube) -> Bool {
+        return x0 <= other.x1 && x1 >= other.x0 &&
+        y0 <= other.y1 && y1 >= other.y0 &&
+        z0 <= other.z1 && z1 >= other.z0
+    }
+    
+    func subtract(cube other: Cube) -> [Cube] {
+        if other.contains(cube: self) {
+            return []
+        }
+        if !intersects(cube: other) {
+            return [self]
+        }
+
+        var xx = [other.x0, other.x1].filter { x0 < $0 && $0 < x1 }
+        var yy = [other.y0, other.y1].filter { y0 < $0 && $0 < y1 }
+        var zz = [other.z0, other.z1].filter { z0 < $0 && $0 < z1 }
+        xx.insert(x0, at: 0)
+        xx.append(x1)
+        yy.insert(y0, at: 0)
+        yy.append(y1)
+        zz.insert(z0, at: 0)
+        zz.append(z1)
+        
+        var result = [Cube]()
+        for i in 0..<(xx.count - 1)  {
+            for j in 0..<(yy.count - 1)  {
+                for k in 0..<(zz.count - 1)  {
+                    let cube = Cube(on: on, x0: xx[i], x1: xx[i+1], y0: yy[j], y1: yy[j+1], z0: zz[k], z1: zz[k+1])
+                    result.append(cube)
+                }
+            }
+        }
+
+        return result.filter { !other.contains(cube: $0) }
+    }
 }
 
 struct Point3D: Hashable, Equatable {
@@ -62,5 +109,18 @@ public func part1() -> Int {
         }
     }
     return world.count
+}
+
+public func part2() -> Int {
+    var world = [Cube]()
+        
+    for c in inputData() {
+        world = world.flatMap { $0.subtract(cube: c) }
+        if c.on {
+            world.append(c)
+        }
+    }
+    
+    return world.reduce(0) { $0 + $1.volume }
 }
 
