7f61d77dd11bcd33a292346c8c25f77bcffac0d3
Lachlan Roche <hello@lachlan.roche.name>
2022-12-02 06:57:02 +1000

    2022 day 1 part 2

--- aoc22.playground/Pages/Day01.xcplaygroundpage/Sources/Day01.swift
+++ aoc22.playground/Pages/Day01.xcplaygroundpage/Sources/Day01.swift
@@ -15,3 +15,17 @@ public func part1() -> Int {
     }
     return most
 }
+
+public func part2() -> Int {
+    var totals = [Int]()
+    var current = 0
+    for line in stringsFromFile() {
+        if line == "" {
+            totals.append(current)
+            current = 0
+        } else if let number = Int(line) {
+            current += number
+        }
+    }
+    return totals.sorted(by: {$0 > $1}).prefix(3).reduce(0, +)
+}
