7db1cede2335a8fb4c870b97e186a652f76dc220
Lachlan Roche <hello@lachlan.roche.name>
2022-12-03 00:53:48 +1000

    2022 day 2 part 2

--- aoc22.playground/Pages/Day02.xcplaygroundpage/Sources/Day02.swift
+++ aoc22.playground/Pages/Day02.xcplaygroundpage/Sources/Day02.swift
@@ -22,3 +22,24 @@ public func part1() -> Int {
     }
     return total
 }
+
+func play(_ a: Int, _ x: Int) -> Int {
+    if x == 1 {
+        return score(a, a == 1 ? 3 : a - 1)
+    } else if x == 2 {
+        return score(a, a)
+    } else {
+        return score(a, a == 3 ? 1 : 1 + a)
+    }
+}
+
+public func part2() -> Int {
+    var total: Int = 0
+    for line in data {
+        guard line != "" else { break }
+        let abc = Int(Array(line)[0].asciiValue!) - 64
+        let xyz = Int(Array(line)[2].asciiValue!) - 87
+        total += play(abc, xyz)
+    }
+    return total
+}
