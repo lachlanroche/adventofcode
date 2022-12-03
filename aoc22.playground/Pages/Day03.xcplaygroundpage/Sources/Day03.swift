cf86684635a9f138cdb2ecf779a375d5a3b6cad1
Lachlan Roche <hello@lachlan.roche.name>
2022-12-03 15:45:28 +1000

    2022 day 3 part 2

--- aoc22.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
+++ aoc22.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
@@ -19,3 +19,23 @@ public func part1() -> Int {
     }
     return total
 }
+
+public func part2() -> Int {
+    var total = 0
+    var elves: [Set<Character>] = []
+    for line in lines {
+        guard line != "" else { break }
+        let carray = Set(Array(line))
+        elves.append(carray)
+        guard elves.count == 3 else { continue }
+        let both = elves[0].intersection(elves[1]).intersection(elves[2])
+        guard let common = both.first else { continue }
+        if common.isUppercase {
+            total += 27 + Int(common.asciiValue!) - 65
+        } else {
+            total += 1 + Int(common.asciiValue!) - 97
+        }
+        elves = []
+    }
+    return total
+}
