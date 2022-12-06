5baec643e66a5277646ba2b9ff66796473f4a6b0
Lachlan Roche <hello@lachlan.roche.name>
2022-12-06 15:14:58 +1000

    2022 day 6 part 2

--- aoc22.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
+++ aoc22.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
@@ -20,3 +20,22 @@ public func part1() -> Int {
     return -1
 }
 
+public func part2() -> Int {
+    let data = stringFromFile()
+    var recent = [Character]()
+    var position = 0
+    for ch in data {
+        if Set(recent).count == 14 {
+            print(ch)
+            print(recent)
+            return position
+        }
+        if recent.count == 14 {
+            recent.remove(at: 0)
+        }
+        recent.append(ch)
+        position += 1
+    }
+    
+    return -1
+}
