989d58c32adcede3fa130f02ab9cbfae425e5afa
Lachlan Roche <hello@lachlan.roche.name>
2022-12-07 15:57:58 +1000

    2022 day 7 part 1

---
+++ aoc22.playground/Pages/Day07.xcplaygroundpage/Sources/Day07.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+public func part1() -> Int {
+    var filesystem: [String:Int] = [
+        "/": 0
+    ]
+    var path = "/"
+    
+    for line in stringsFromFile() {
+        guard line != "" else { break }
+        if line == "$ cd /" {
+            path = "/"
+        } else if line == "$ cd .." {
+            path = path.split(separator: "/").dropLast().joined(separator: "/")
+            if !path.hasPrefix("/") {
+                path = "/" + path
+            }
+            if !path.hasSuffix("/") {
+                path = path + "/"
+            }
+        } else if line.hasPrefix("$ cd ") {
+            let subdir = line.split(separator: " ").last!
+            path = path + subdir + "/"
+        } else if line == "$ ls" {
+            // nothing
+        } else {
+            let parts = line.split(separator: " ")
+            if parts[0] == "dir" {
+                filesystem[path + parts[1] + "/"] = 0
+            } else {
+                filesystem[path + parts[1]] = Int(parts[0])!
+            }
+        }
+    }
+    
+    var sizes = [String:Int]()
+    for dir in filesystem.keys where dir.hasSuffix("/") {
+        sizes[dir] = filesystem.filter({ $0.key.hasPrefix(dir) })
+            .reduce(0, { $0 + $1.value })
+    }
+    
+    return sizes.filter({ $0.value <= 100000 })
+        .reduce(0, { $0 + $1.value })
+}
