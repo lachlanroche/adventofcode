a49faf59122a2ea0ef18b0403b269609ea3577dc
Lachlan Roche <hello@lachlan.roche.name>
2022-12-08 21:34:44 +1000

    2022 day 8 part 2

--- aoc22.playground/Pages/Day08.xcplaygroundpage/Sources/Day08.swift
+++ aoc22.playground/Pages/Day08.xcplaygroundpage/Sources/Day08.swift
@@ -70,3 +70,53 @@ public func part1() -> Int {
     }
     return visible.count
 }
+
+public func part2() -> Int {
+    var maxX = 0
+    var maxY = 0
+    var world = [Point:Int]()
+    for (y, line) in stringsFromFile().enumerated() {
+        guard line != "" else { break }
+        maxY = max(maxY, y)
+        for (x, ch) in line.enumerated() {
+            maxX = max(maxX, x)
+            world[Point(x: x, y: y)] = ch.wholeNumberValue!
+        }
+    }
+    
+    func trees(_ point: Point, _ check: [Point] ) -> Int {
+        let max = world[point]!
+        var result = 0
+        for c in check {
+            result += 1
+            let h = world[c]!
+            if h >= max {
+                break
+            }
+        }
+        return result
+    }
+
+    var result = 0
+    for x in 0...maxX {
+        for y in 0...maxY {
+            let p = Point(x: x, y: y)
+            var score = 1
+            if x != maxX {
+                score *= trees(p, ((x+1)...maxX).map({ Point(x: $0, y: y) }))
+            }
+            if x != 0 {
+                score *= trees(p, (0...(x-1)).reversed().map({ Point(x: $0, y: y) }))
+            }
+            if y != maxY {
+                score *= trees(p, ((y+1)...maxY).map({ Point(x: x, y: $0) }))
+            }
+            if y != 0 {
+                score *= trees(p, (0...(y-1)).reversed().map({ Point(x: x, y: $0) }))
+            }
+            result = max(result, score)
+        }
+    }
+
+    return result
+}
