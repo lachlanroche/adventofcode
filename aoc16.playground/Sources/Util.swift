ebb886ed809cf98bc1e7f418dfc9abadc65d865b
Lachlan Roche <hello@lachlan.roche.name>
2022-12-10 11:38:11 +1000

    2016 setup

---
+++ aoc16.playground/Sources/Util.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+public func stringFromFile(named: String = "input") -> String
+{
+    guard let path = Bundle.main.path(forResource: named, ofType: "txt") else { return "" }
+    guard let data = FileManager.default.contents(atPath: path) else { return "" }
+    guard let content = String(data:data, encoding:String.Encoding.utf8) else { return "" }
+    
+    return content
+}
+
+public func stringsFromFile(named: String = "input") -> [String]
+{
+    return stringFromFile(named: named)
+        .components(separatedBy: CharacterSet.newlines)
+}
+
+public func numbersFromFile(named: String = "input") -> [Int]
+{
+    return stringsFromFile(named: named)
+        .compactMap(Int.init)
+}
+
+public func stringsFromString(_ str: String, sep: String = ",") -> [String]
+{
+    return str.components(separatedBy: CharacterSet.whitespacesAndNewlines.union(CharacterSet.init(charactersIn: sep)))
+}
+
+public func numbersFromString(_ str: String, sep: String = ",") -> [Int]
+{
+    return stringsFromString(str, sep: sep)
+        .compactMap(Int.init)
+}
