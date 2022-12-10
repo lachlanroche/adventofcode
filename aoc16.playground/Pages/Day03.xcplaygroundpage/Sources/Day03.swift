af5290d2a292fabb246a07cc47fc262415952cb2
Lachlan Roche <hello@lachlan.roche.name>
2022-12-10 12:28:45 +1000

    2016 day 3 part 2

--- aoc16.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
+++ aoc16.playground/Pages/Day03.xcplaygroundpage/Sources/Day03.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 public func part1() -> Int {
     var result = 0
     for line in stringsFromFile() {
@@ -17,3 +16,40 @@ public func part1() -> Int {
     return result
 }
 
+public func part2() -> Int {
+    var result = 0
+    var a = [Int]()
+    var b = [Int]()
+    var c = [Int]()
+    for line in stringsFromFile() {
+        guard line != "" else { break }
+        let sides = [
+            Int(line[0..<5].trimmingCharacters(in: .whitespaces))!,
+            Int(line[5..<10].trimmingCharacters(in: .whitespaces))!,
+            Int(line[10..<15].trimmingCharacters(in: .whitespaces))!
+        ]
+        a.append(sides[0])
+        b.append(sides[1])
+        c.append(sides[2])
+        if a.count == 3 {
+            let aa = a.sorted()
+            if aa[0] + aa[1] > aa[2] {
+                result += 1
+            }
+            let bb = b.sorted()
+            if bb[0] + bb[1] > bb[2] {
+                result += 1
+            }
+            let cc = c.sorted()
+            if cc[0] + cc[1] > cc[2] {
+                result += 1
+            }
+            a = []
+            b = []
+            c = []
+        }
+        
+        
+    }
+    return result
+}
