6983ac6c5c0e79ebe06b63dfb6c75ad8bbef78dc
Lachlan Roche <hello@lachlan.roche.name>
2022-12-10 20:07:18 +1000

    2016 day 6 part 2

--- aoc16.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
+++ aoc16.playground/Pages/Day06.xcplaygroundpage/Sources/Day06.swift
@@ -4,7 +4,6 @@ public func part1() -> String {
     var counts: Array<Dictionary<Character,Int>> = [
         [:], [:], [:], [:], [:], [:], [:], [:]
     ]
-    // nokeeods
     for line in stringsFromFile() where line != "" {
         for (i, ch) in line.enumerated() {
             counts[i][ch] = 1 + (counts[i][ch] ?? 0)
@@ -26,3 +25,27 @@ public func part1() -> String {
     return String(result)
 }
 
+public func part2() -> String {
+    var counts: Array<Dictionary<Character,Int>> = [
+        [:], [:], [:], [:], [:], [:], [:], [:]
+    ]
+    for line in stringsFromFile() where line != "" {
+        for (i, ch) in line.enumerated() {
+            counts[i][ch] = 1 + (counts[i][ch] ?? 0)
+        }
+    }
+    
+    let result: [Character] = counts.map {
+        var count = Int.max
+        var result: Character = "_"
+        for (ch, c) in $0 {
+            if c < count {
+                count = c
+                result = ch
+            }
+        }
+        return result
+    }
+    
+    return String(result)
+}
