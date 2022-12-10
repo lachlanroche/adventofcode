53909129aa13b1de8e03184860d4fc4d8ec52935
Lachlan Roche <hello@lachlan.roche.name>
2022-12-10 19:33:09 +1000

    2016 day 4 part 2

--- aoc16.playground/Pages/Day04.xcplaygroundpage/Sources/Day04.swift
+++ aoc16.playground/Pages/Day04.xcplaygroundpage/Sources/Day04.swift
@@ -22,3 +22,42 @@ public func part1() -> Int {
     }
     return result
 }
+
+extension Character {
+    func rot13(_ n: Int) -> Character {
+        guard
+            let asciiValue = asciiValue,
+            97...122 ~= asciiValue
+        else { return self }
+        
+        return Character(UnicodeScalar(97+(Int(asciiValue) - 97 + n) % 26)!)
+    }
+}
+
+public func part2() -> Int {
+    for line in stringsFromFile() where line != "" {
+        let prefix = line.split(separator: "[")[0]
+        let id = Int(prefix.split(separator: "-").last!)!
+        let checksum = line.split(separator: "[")[1].replacingOccurrences(of: "]", with: "")
+        var counts = Dictionary<Character, Int>()
+        for p in prefix.filter({ $0.isLetter }) {
+            counts[p] = 1 + (counts[p] ?? 0)
+            
+        }
+        var values = [(Int, Int, Character)]()
+        for (k, v) in counts {
+            values.append((v, Int(k.asciiValue!), k))
+        }
+        values.sort(by: { $0.0 == $1.0 ? ($0.1 < $1.1) : $0.0 > $1.0 })
+        guard String(values.map({ $0.2 })).hasPrefix(checksum) else { continue }
+        
+        let name = prefix.filter({ $0.isLetter || $0 == "-"})
+            .map({ $0 == "-" ? " " : $0 })
+            .map({ $0.rot13(id) })
+        
+        if String(name).contains("north") {
+            return id
+        }
+    }
+    return 0
+}
