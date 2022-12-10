1853b51e4f3848bb6372b03487a9d1b762f4a131
Lachlan Roche <hello@lachlan.roche.name>
2022-12-10 15:24:27 +1000

    2022 day 10 part 2

--- aoc22.playground/Pages/Day10.xcplaygroundpage/Sources/Day10.swift
+++ aoc22.playground/Pages/Day10.xcplaygroundpage/Sources/Day10.swift
@@ -22,3 +22,33 @@ public func part1() -> Int {
         result[180] * 180 +
         result[220] * 220
 }
+
+public func part2() -> Void {
+    var result: [Int] = []
+    var x = 1
+    for line in stringsFromFile() {
+        guard line != "" else { break }
+        if line == "noop" {
+            result.append(x)
+        } else if line.hasPrefix("addx") {
+            let v = Int(line.split(separator: " ").last!)!
+            result.append(x)
+            result.append(x)
+            x += v
+        }
+    }
+    
+    var screen = ""
+    for (i, signal) in result.enumerated() {
+        if i%40 == 0 && i != 0 {
+            screen.append("\n")
+        }
+        let x = i % 40
+        if (x-1)...(x+1) ~= signal {
+            screen.append("#")
+        } else {
+            screen.append(".")
+        }
+    }
+    print(screen)
+}
