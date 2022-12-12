f7ab996a713e53629d0fadb481423749b17fdc06
Lachlan Roche <hello@lachlan.roche.name>
2022-12-12 23:13:31 +1000

    2022 day 12 part 1

---
+++ aoc22.playground/Pages/Day12.xcplaygroundpage/Sources/Day12.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+struct Point: Hashable, Equatable {
+    let x: Int
+    let y: Int
+    
+    func manhattan(_ p: Point) -> Int {
+        abs(p.x - x) + abs(p.y - y)
+    }
+    
+    func neighbors() -> [Point] {
+        return [
+            Point(x: x - 1, y: y),
+            Point(x: x + 1, y: y),
+            Point(x: x, y: y - 1),
+            Point(x: x, y: y + 1),
+        ]
+    }
+}
+
+func bfs(start: Point, goal: (Point) -> Bool, world: Dictionary<Point, Int>) -> [Point] {
+    var queue = [Point]()
+    queue.append(start)
+    var cameFrom = Dictionary<Point, Point>()
+
+    while !queue.isEmpty {
+        let cur = queue.remove(at: 0)
+        if goal(cur) {
+            var result = [Point]()
+            var prev: Point? = cur
+            while prev != nil && prev != start {
+                result.append(prev!)
+                prev = cameFrom[prev!]
+            }
+            return result.reversed()
+        }
+        for n in cur.neighbors() where world[n] != nil && world[n]! <= world[cur]! + 1 {
+            guard cameFrom[n] == nil else { continue }
+            cameFrom[n] = cur
+            queue.append(n)
+        }
+    }
+    return []
+}
+
+public func part1() -> Int {
+    var world = Dictionary<Point, Int>()
+    var start = Point(x: 0, y: 0)
+    var goal = Point(x: 0, y: 0)
+    var y = 0
+    for line in stringsFromFile() where line != "" {
+        var x = 0
+        for ch in line {
+            let p = Point(x: x, y: y)
+            var value = Int(ch.asciiValue!) - 97
+            if ch == "S" {
+                start = p
+                value = 0
+            } else if ch == "E" {
+                goal = p
+                value = 25
+            }
+            world[p] = value
+            x += 1
+        }
+        y += 1
+    }
+    return bfs(start: start, goal: { $0 == goal }, world: world).count
+}
