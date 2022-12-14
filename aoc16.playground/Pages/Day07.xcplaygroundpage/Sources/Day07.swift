d07379bd71c439c92e422ee540ff4d88a3d6e7be
Lachlan Roche <hello@lachlan.roche.name>
2022-12-14 20:37:31 +1000

    2016 day 7 part 2

--- aoc16.playground/Pages/Day07.xcplaygroundpage/Sources/Day07.swift
+++ aoc16.playground/Pages/Day07.xcplaygroundpage/Sources/Day07.swift
@@ -35,3 +35,39 @@ public func part1() -> Int {
     }
     return result
 }
+
+public func part2() -> Int {
+    var result = 0
+    for line in stringsFromFile() where line != "" {
+        var square = false
+        var aba = Set<String>()
+        var bab = Set<String>()
+        var buffer = [Character]()
+        
+        for ch in line {
+            if square && ch == "]" {
+                buffer = []
+                square = false
+            } else if !square && ch == "[" {
+                buffer = []
+                square = true
+            } else {
+                buffer.append(ch)
+                if buffer.count == 3 {
+                    if buffer[0] != buffer[1] && buffer[0] == buffer[2] {
+                        if square {
+                            aba.insert(String(buffer.dropFirst(1)))
+                        } else {
+                            bab.insert(String(buffer.dropLast(1)))
+                        }
+                    }
+                    buffer.remove(at: 0)
+                }
+            }
+        }
+        if !aba.isEmpty && !aba.intersection(bab).isEmpty {
+            result += 1
+        }
+    }
+    return result
+}
