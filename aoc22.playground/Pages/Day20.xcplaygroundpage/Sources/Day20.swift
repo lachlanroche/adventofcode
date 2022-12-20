dedf4664cdb743e94d79d529a912e4daa8aa243e
Lachlan Roche <hello@lachlan.roche.name>
2022-12-20 22:41:33 +1000

    2022 day 20 part 2

--- aoc22.playground/Pages/Day20.xcplaygroundpage/Sources/Day20.swift
+++ aoc22.playground/Pages/Day20.xcplaygroundpage/Sources/Day20.swift
@@ -21,3 +21,23 @@ public func part1() -> Int {
     }
     return -1
 }
+
+public func part2() -> Int {
+    var list = inputData()
+    let length = list.count
+    for _ in 1...10 {
+        for i in 0..<length {
+            guard let index = list.firstIndex(where: { $0.i == i }) else { continue }
+            let item = list[index]
+            list.remove(at: index)
+            let value = 811589153 * item.value
+            let extra = value > 0 ? 0 : (length - 1) * (1 + abs(value).quotientAndRemainder(dividingBy: length - 1).quotient)
+            let newIndex = (index + value + extra) % (length - 1)
+            list.insert(item, at: newIndex)
+        }
+    }
+    if let index = list.firstIndex(where: { $0.value == 0 }) {
+        return [1000, 2000, 3000].reduce(0) { $0 + 811589153 * list[(index + $1) % length].value }
+    }
+    return -1
+}
