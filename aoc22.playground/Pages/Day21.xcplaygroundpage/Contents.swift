37ceb3aac8bf5d270e84c8b4ffceb0e448fc57f1
Lachlan Roche <hello@lachlan.roche.name>
2022-12-21 20:50:50 +1000

    2022 day 21 part 2

--- aoc22.playground/Pages/Day21.xcplaygroundpage/Contents.swift
+++ aoc22.playground/Pages/Day21.xcplaygroundpage/Contents.swift
@@ -3,5 +3,7 @@
 import Foundation
 
 part1()
+part2()
+part3()
 
 //: [Next](@next)
--- aoc22.playground/Pages/Day21.xcplaygroundpage/Sources/Day21.swift
+++ aoc22.playground/Pages/Day21.xcplaygroundpage/Sources/Day21.swift
@@ -2,6 +2,7 @@ import Foundation
 
 enum Monkey {
     case constant(Int)
+    case equal(String, String)
     case add(String, String)
     case subtract(String, String)
     case multiply(String, String)
@@ -34,6 +35,7 @@ public func part1() -> Int {
         guard let chimp = monkeys[id] else { return 0 }
         switch chimp {
         case let .constant(c): return c
+        case let .equal(a, b): return monkey(a) == monkey(b) ? 1 : 0
         case let .add(a, b): return monkey(a) + monkey(b)
         case let .subtract(a, b): return monkey(a) - monkey(b)
         case let .multiply(a, b): return monkey(a) * monkey(b)
@@ -44,3 +46,55 @@ public func part1() -> Int {
     let monkeys = inputData()
     return monkey("root")
 }
+
+public func part2() -> Int {
+    func monkey(_ id: String) -> Int {
+        guard let chimp = monkeys[id] else { return 0 }
+        switch chimp {
+        case let .constant(c): return c
+        case let .equal(a, b): return monkey(a) == monkey(b) ? 1 : 0
+        case let .add(a, b): return monkey(a) + monkey(b)
+        case let .subtract(a, b): return monkey(a) - monkey(b)
+        case let .multiply(a, b): return monkey(a) * monkey(b)
+        case let .divide(a, b): return monkey(a) / monkey(b)
+        }
+    }
+    func show(_ id: String) -> String {
+        guard let chimp = monkeys[id] else { return "" }
+        if id == "humn" { return "x" }
+        switch chimp {
+        case let .constant(c): return String(c)
+        case let .equal(a, b): return "(\(show(a))) = (\(show(b)))"
+        case let .add(a, b): return "(\(show(a)) + \(show(b)))"
+        case let .subtract(a, b): return "(\(show(a)) - \(show(b)))"
+        case let .multiply(a, b): return "(\(show(a)) * \(show(b)))"
+        case let .divide(a, b): return "(\(show(a)) / \(show(b)))"
+        }
+        
+    }
+    
+    var monkeys = inputData()
+    monkeys["root"] = .equal("qpct", "dthc")
+    print(show("root"))
+    return 0
+}
+
+public func part3() -> Int {
+    func monkey(_ id: String) -> Int {
+        guard let chimp = monkeys[id] else { return 0 }
+        switch chimp {
+        case let .constant(c): return c
+        case let .equal(a, b): return monkey(a) == monkey(b) ? 1 : 0
+        case let .add(a, b): return monkey(a) + monkey(b)
+        case let .subtract(a, b): return monkey(a) - monkey(b)
+        case let .multiply(a, b): return monkey(a) * monkey(b)
+        case let .divide(a, b): return monkey(a) / monkey(b)
+        }
+    }
+    var monkeys = inputData()
+    monkeys["root"] = .equal("qpct", "dthc")
+    monkeys["humn"] = .constant(3592056845086)
+    return monkey("root")
+}
+
+
